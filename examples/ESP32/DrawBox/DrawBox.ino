0e898329dc2e6ee4697c9c160b5c6ce18a908609
H1Jam <hosseinjamshidien@gmail.com>
2023-07-22 02:36:44 -0400

    Update DrawBox.ino

--- examples/ESP32/DrawBox/DrawBox.ino
+++ examples/ESP32/DrawBox/DrawBox.ino
@@ -13,17 +13,33 @@ void addBoxes() {
   screenRectangle1.cmdId = ADD_OR_REFRESH_CMD;
   screenRectangle1.width = 150;
   screenRectangle1.height = 100;
-  screenRectangle1.cornersRadius = 131;
+  screenRectangle1.fillColor = UBUNTU;
+  screenRectangle1.strokeColor = YELLOW;
+  screenRectangle1.strokeWidth = 2;
+  screenRectangle1.cornersRadius = 5;
   sendScreenStream(&screenRectangle1, &SerialBT);
   
-  screenRectangle2.x = 160;
+  screenRectangle2.x = 170;
   screenRectangle2.y = 10;
   screenRectangle2.cmdId = ADD_OR_REFRESH_CMD;
   screenRectangle2.width = 150;
   screenRectangle2.height = 100;
-  screenRectangle2.cornersRadius = 131;
+  screenRectangle2.fillColor = UBUNTU;
+  screenRectangle2.strokeColor = YELLOW;
+  screenRectangle2.strokeWidth = 2;
+  screenRectangle2.cornersRadius = 10;
   sendScreenStream(&screenRectangle2, &SerialBT);
   
+  screenRectangle3.x = 10;
+  screenRectangle3.y = 120;
+  screenRectangle3.cmdId = ADD_OR_REFRESH_CMD;
+  screenRectangle3.width = 310;
+  screenRectangle3.height = 200;
+  screenRectangle3.fillColor = UBUNTU;
+  screenRectangle3.strokeColor = YELLOW;
+  screenRectangle3.strokeWidth = 2;
+  screenRectangle3.cornersRadius = 10;
+  sendScreenStream(&screenRectangle3, &SerialBT);
 }
 
 void screenSetup()
