bfa879248535459b80e3e88ce1bf24ff25e1bc5b
H1Jam <hosseinjamshidien@gmail.com>
2023-07-22 01:10:47 -0400

    Update Joystick.ino

--- examples/ESP32/Joystick/Joystick.ino
+++ examples/ESP32/Joystick/Joystick.ino
@@ -42,7 +42,8 @@ void setup() {
 
 void loop() {
   while (SerialBT.available()) {
-  screenObjects.updateScreen(SerialBT.read());
-  delay(1);
+    screenObjects.updateScreen(SerialBT.read());
   }
+  delay(2);
 }
+
