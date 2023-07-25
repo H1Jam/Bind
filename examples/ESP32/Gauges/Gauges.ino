98f3e9d27df9bbdc194002b41a5c3b4ae7a7af75
H1Jam <hosseinjamshidien@gmail.com>
2023-07-25 02:06:40 -0400

    Update Gauges.ino

--- examples/ESP32/Gauges/Gauges.ino
+++ examples/ESP32/Gauges/Gauges.ino
@@ -11,31 +11,79 @@ int counter = 0;
 float g1 = 0;
 float g2 = 0;
 
-float g1Delta = 1;
+float gDelta = 1;
 
-void setAttitudeIndicator(float roll, float pitch) {
-  screenAttitudeIndicator.x = 10;
-  screenAttitudeIndicator.y = 10;
-  screenAttitudeIndicator.cmdId = ADD_OR_REFRESH_CMD;
-  screenAttitudeIndicator.roll = roll;
-  screenAttitudeIndicator.pitch = pitch;
-  screenAttitudeIndicator.dimSize = 150;
-  sendScreenStream(&screenAttitudeIndicator, &SerialBT);
+
+void addGauge(float value)
+{
+  screenGauge.x = 120;
+  screenGauge.y = 500;
+  screenGauge.dimSize = 120;
+  screenGauge.value = value;
+  screenGauge.maxValue = 200.0f;
+  screenGauge.drawArc = 100;
+  screenGauge.arcGreenMaxVal = 100.0f;
+  screenGauge.arcYellowMaxVal = 150.0f;
+  screenGauge.arcRedMaxVal = 180.0f;
+  screenGauge.setlabel("Celcesius");
+  screenGauge.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenGauge, &SerialBT);
 }
 
-void setHeadingIndicator(float heading) {
-  screenHeadingIndicator.x = 190;
-  screenHeadingIndicator.y = 10;
-  screenHeadingIndicator.cmdId = ADD_OR_REFRESH_CMD;
-  screenHeadingIndicator.heading = heading;
-  screenHeadingIndicator.dimSize = 150;
-  sendScreenStream(&screenHeadingIndicator, &SerialBT);
+void updateGauge(float value)
+{
+  screenGauge.value = value;
+  screenGauge.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenGauge, &SerialBT);
+}
+
+void addnGaugeCompact(float value) {
+  screenGaugeCompact.x = 20;
+  screenGaugeCompact.y = 270;
+  screenGaugeCompact.dimSize = 120;
+  screenGaugeCompact.value = value;
+  screenGaugeCompact.maxValue = 200.0f;
+  screenGaugeCompact.drawArc = 100;
+  screenGaugeCompact.arcGreenMaxVal = 100.0f;
+  screenGaugeCompact.arcYellowMaxVal = 150.0f;
+  screenGaugeCompact.arcRedMaxVal = 180.0f;
+  screenGaugeCompact.setlabel("Temp Celcesius");
+  screenGaugeCompact.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenGaugeCompact, &SerialBT);
+}
+
+void updateGaugeCompact(float value) {
+  screenGaugeCompact.value = value;
+  screenGaugeCompact.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenGaugeCompact, &SerialBT);
+}
+
+void addGaugeSimple(float value) {
+  screenGaugeSimple.x = 129;
+  screenGaugeSimple.y = 150;
+  screenGaugeSimple.dimSize = 153;
+  screenGaugeSimple.value = value;
+  screenGaugeSimple.maxValue = 250;
+  screenGaugeSimple.minValue = 175;
+  screenGaugeSimple.style = 0;
+  screenGaugeSimple.isSymmetrical = true;
+  screenGaugeSimple.color = BLUE;
+  screenGaugeSimple.setlabel("Engine RPM");
+  screenGaugeSimple.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenGaugeSimple, &SerialBT);
+}
+
+void updateGaugeSimple(float value) {
+  screenGaugeSimple.value = value;
+  screenGaugeSimple.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenGaugeSimple, &SerialBT);
 }
 
 void screenSetup() {
   Serial.println("Screen setup started!");
-  setAttitudeIndicator(0.0f, 0.0f);
-  setHeadingIndicator(0.0f);
+  addGauge(0.0f);
+  addnGaugeCompact(0.0f);
+  addGaugeSimple(0.0f);
   Serial.println("Screen setup done!");
 }
 
@@ -57,22 +105,15 @@ void loop() {
   counter++;
   if (counter > 20) {
     counter = 0;
-    rollC += rollDelta;
-    pitchC += pitchDelta;
-    headingC += 5;
+    g1 += gDelta;
+    g2 -= gDelta;
 
-    if (rollC > 20 || rollC < -20) {
-      rollDelta = -1 * rollDelta;
+    if (g1 > 20 || g1 < -20) {
+      gDelta = -1 * gDelta;
     }
 
-    if (pitchC > 20 || pitchC < -20) {
-      pitchDelta = -1 * pitchDelta;
-    }
-
-    if (headingC > 360) {
-      headingC = 0.0f;
-    }
-    setAttitudeIndicator(rollC, pitchC);
-    setHeadingIndicator(headingC);
+  updateGauge(0.0f);
+  updateGaugeCompact(0.0f);
+  updateGaugeSimple(0.0f);
   }
 }
