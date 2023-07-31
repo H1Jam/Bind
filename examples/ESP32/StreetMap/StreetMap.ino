e2d61c09d91554f72309db27cbfa58c80f433e25
H1Jam <hosseinjamshidien@gmail.com>
2023-07-30 23:12:50 -0400

    Update StreetMap.ino

--- examples/ESP32/StreetMap/StreetMap.ino
+++ examples/ESP32/StreetMap/StreetMap.ino
@@ -3,7 +3,9 @@
 
 BluetoothSerial SerialBT;
 ScreenObjects screenObjects;
-ScreenChart screenChart;
+ScreenMap screenMap;
+ScreenMapMarker screenMapMarker1;
+ScreenMapMarker screenMapMarker2;
 
 int counter = 0;
 float dummyData = 0.0f;
@@ -13,19 +15,38 @@ void sendDummyChartdata() {
   addChartdata(dummyData, &screenChart, &SerialBT);
 }
 
-void addChart() {
-  screenChart.x = 10;
-  screenChart.y = 10;
-  screenChart.cmdId = ADD_OR_REFRESH_CMD;
-  screenChart.width = 250;
-  screenChart.height = 140;
-  screenChart.maxY = 10;
-  screenChart.minY = -10;
-  screenChart.maxX = 20;
-  screenChart.autoSize = true;
-  screenChart.color = YELLOW;
-  sendScreenStream(&screenChart, &SerialBT);
+void addMap()
+{
+  screenMap.cmdId = ADD_OR_REFRESH_CMD;
+  screenMap.x = 10;
+  screenMap.y = 10;
+  screenMap.width = 300;
+  screenMap.height = 200;
+  screenMap.lat = 43.730825f;
+  screenMap.lon = -79.440881f;
+  screenMap.mapOrientation = 0.0f;
+  screenMap.zoom = 16;
+  sendScreenStream(&screenMap, &SerialBT);
 }
+
+
+void addMapMarker()
+{
+  gVal += 10;
+  screenMapMarker1.lat = 43.732825f;
+  screenMapMarker1.lon = -79.442881f;
+  screenMapMarker1.rotation = gVal;
+  screenMapMarker1.iconId = 1;
+  screenMapMarker1.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenMapMarker1, &SerialBT);
+  screenMapMarker2.lat = 43.733825f;
+  screenMapMarker2.lon = -79.443881f;
+  screenMapMarker2.rotation = gVal;
+  screenMapMarker2.iconId = 2;
+  screenMapMarker2.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&screenMapMarker2, &SerialBT);
+}
+
 
 void screenSetup() {
   Serial.println("Screen setup started!");
