12bbd8d356ea005f69db9fc6c809deb5594d882d
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-08-09 00:53:12 -0400

    Update RC_Controller.ino

--- examples/ESP32/Complex Examples/RC_Controller/RC_Controller.ino
+++ examples/ESP32/Complex Examples/RC_Controller/RC_Controller.ino
@@ -3,35 +3,53 @@
 
 BluetoothSerial SerialBT;
 ScreenObjects screenObjects;
-ScreenJoystick joystick1;
+ScreenJoystick joystickRight;
+ScreenJoystick joystickLeft;
 
-void joystick1_Callback(int16_t x, int16_t y)
+void joystickRight_Callback(int16_t x, int16_t y)
 {
-  Serial.print("joystick1 has been changed, x:");
+  Serial.print("joystickRight has been changed, x:");
   Serial.print(x);
   Serial.print(" y:");
   Serial.println(y);
 }
 
-void addJoystick() {
-  joystick1.x = 50;
-  joystick1.y = 80;
-  joystick1.cmdId = ADD_OR_REFRESH_CMD;
-  joystick1.dimSize = 200;
-  sendScreenStream(&joystick1, &SerialBT);
+void joystickLeft_Callback(int16_t x, int16_t y)
+{
+  Serial.print("joystickLeft has been changed, x:");
+  Serial.print(x);
+  Serial.print(" y:");
+  Serial.println(y);
+}
+
+void addJoystickRight() {
+  joystickRight.x = 50;
+  joystickRight.y = 80;
+  joystickRight.cmdId = ADD_OR_REFRESH_CMD;
+  joystickRight.dimSize = 200;
+  sendScreenStream(&joystickRight, &SerialBT);
+}
+
+void addJoystickLeft() {
+  joystickLeft.x = 50;
+  joystickLeft.y = 80;
+  joystickLeft.cmdId = ADD_OR_REFRESH_CMD;
+  joystickLeft.dimSize = 200;
+  sendScreenStream(&joystickLeft, &SerialBT);
 }
 
 void screenSetup()
 {
   Serial.println("Screen setup started!");
-  addJoystick();
+  addJoystick1();
   Serial.println("Screen setup done!");
 }
 
 void setup() {
   Serial.begin(115200);
   screenObjects.registerScreenSetup(&screenSetup);
-  screenObjects.registerJoystick(&joystick1, &joystick1_Callback);
+  screenObjects.registerJoystick(&joystickRight, &joystickRight_Callback);
+  screenObjects.registerJoystick(&joystickLeft, &joystickLeft_Callback);
   String devName = "ESP32testB";
   SerialBT.begin(devName);
   Serial.println("The bluetooth device started, now you can pair the phone with bluetooth!");
