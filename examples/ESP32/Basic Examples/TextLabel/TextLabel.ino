f62f173d7d1db1a27400e61180334014e3e8d009
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-08-09 00:52:28 -0400

    Create TextLabel.ino

---
+++ examples/ESP32/Basic Examples/TextLabel/TextLabel.ino
@@ -0,0 +1,60 @@
+#include "BluetoothSerial.h"
+#include "Bind.hpp"
+
+BluetoothSerial SerialBT;
+ScreenObjects screenObjects;
+ScreenTextLabel textLabel1;
+ScreenTextLabel textLabel2;
+int counter = 0;
+char buffer[10];
+
+void addtTextlabel() {
+  textLabel1.x = 20;
+  textLabel1.y = 20;
+  textLabel1.setlabel("Hello Android!");
+  textLabel1.color = WHITE;
+  textLabel1.fontSize = 28;
+  textLabel1.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&textLabel1, &SerialBT);
+
+  textLabel2.x = 20;
+  textLabel2.y = 50;
+  textLabel2.setlabel("Second caption");
+  textLabel2.color = YELLOW;
+  textLabel2.fontSize = 18;
+  textLabel2.cmdId = ADD_OR_REFRESH_CMD;
+  sendScreenStream(&textLabel2, &SerialBT);
+}
+
+void screenSetup()
+{
+  Serial.println("Screen setup started!");
+  addtTextlabel();
+  Serial.println("Screen setup done!");
+}
+
+void setup() {
+  Serial.begin(115200);
+  screenObjects.registerScreenSetup(&screenSetup);
+  String devName = "ESP32testB";
+  SerialBT.begin(devName);
+  Serial.println("The bluetooth device started, now you can pair the phone with bluetooth!");
+  Serial.println("devName:");
+  Serial.println(devName);
+}
+
+void loop() {
+  while (SerialBT.available()) {
+    screenObjects.updateScreen(SerialBT.read());
+  }
+  delay(10);
+  counter++;
+  if(counter>100){
+    counter=0;
+    snprintf(buffer, 10, "%d", millis());
+    textLabel2.setlabel(buffer);
+    textLabel2.cmdId = ADD_OR_REFRESH_CMD;
+    sendScreenStream(&textLabel2, &SerialBT);
+  }
+}
+
