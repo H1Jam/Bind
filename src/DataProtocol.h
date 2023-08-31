0d3aa16e2940e2ba70d70a38b0e6d0c42b3d2f63
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-08-31 06:06:05 -0400

    Update DataProtocol.h

--- src/DataProtocol.h
+++ src/DataProtocol.h
@@ -2,9 +2,13 @@
 #define __DATAPROTOCOL_H
 #include <stdint.h>
 #include <string.h>
+#include "Stream.h"
+#include "SLPacker.hpp"
 #define MAX_DATA_LENGHT  100
 #define RGB(r, g ,b) ((int32_t) ((uint8_t) (b)) | ((int32_t) (g) << 8) | (((int32_t) (uint8_t) (r)) << 16) | 0xFF000000)
 #define RGBA(r, g ,b, a) ((int32_t) ((uint8_t) (b)) | ((int32_t) (g) << 8) | (((int32_t) (uint8_t) (r)) << 16) | ((int32_t) ((uint8_t) (a)) << 24))
+
+
 
 static uint8_t VERSIONID = 0;
 
@@ -90,12 +94,15 @@ int sendFrame(uint8_t *destBuffer, uint8_t const *buffer, size_t len);
 bool checkCRC(uint8_t const *buffer, size_t len);
 class DataParser {
   private:
+	SLPacker slPacker;
     uint8_t dataIn[3] = {0, 0, 0};
     int bufIndex = 0;
     int dataStep = 0;
     int dataLength = 0;
   public:
-    uint8_t buf[MAX_DATA_LENGHT];
+	static size_t sendFrame(uint8_t *destBuffer, uint8_t const *buffer, size_t len);
+	static size_t sendFrame(uint8_t *destBuffer, uint8_t const *buffer, size_t len, Stream *stream);
+    uint8_t *buf = slPacker.buf;
     int update(uint8_t inp);
 };
 
