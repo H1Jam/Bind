a4601ed8eacdb6ce6e52b9ec9839676b33829700
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-17 03:16:13 -0400

    Update SLPacker.hpp

--- src/SLPacker.hpp
+++ src/SLPacker.hpp
@@ -3,18 +3,20 @@
 #include <stdint.h>
 #include <string.h>
 #include <stddef.h>
-#define MAX_DATA_LENGHT  100
+#define MAX_DATA_LENGHT 100
 
-class SLPacker {
-  private:
-	enum SLSteps {
-		UNDEF, 
-		GET_CMD, 
-		GET_DATA, 
+class SLPacker
+{
+private:
+	enum SLSteps
+	{
+		UNDEF,
+		GET_CMD,
+		GET_DATA,
 		ESC_DATA
 	};
-    size_t bufIndex = 0;
-    SLSteps slStep = UNDEF;
+	size_t bufIndex = 0;
+	SLSteps slStep = UNDEF;
 	static const uint8_t FEND = 0xC0;
 	static const uint8_t FESC = 0xDB;
 	static const uint8_t TFEND = 0xDC;
@@ -24,11 +26,12 @@ class SLPacker {
 	void resetState(uint8_t data);
 	size_t dataHandler(uint8_t data);
 	void escDataHandler(uint8_t data);
-  public:
-    uint8_t buf[MAX_DATA_LENGHT*2];
+
+public:
+	uint8_t buf[MAX_DATA_LENGHT * 2];
 	size_t encode(uint8_t *writeBuffer, uint8_t const *readBuffer, size_t len);
 	static size_t encode(uint8_t *readBuffer, size_t len);
-    size_t decode(uint8_t data);
+	size_t decode(uint8_t data);
 };
 
 #endif /* __SLPACKER_H */
