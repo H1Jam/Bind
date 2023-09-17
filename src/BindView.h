85152c31d8adc7eaeb05b865965ec63b6ccfb36b
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-17 02:40:36 -0400

    Create BindView.h

---
+++ src/BindView.h
@@ -0,0 +1,10 @@
+#ifndef __BINDVIEW_H
+#define __BINDVIEW_H
+
+class BindView {
+  public:
+    virtual uint16_t getBytes(uint8_t *out) {return 0;};
+    int16_t tag;
+};
+
+#endif /* __BINDVIEW_H */
