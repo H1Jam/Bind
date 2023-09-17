1b5a5823ac82c286a9e9d11f9f98ab61761259a4
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-17 03:16:26 -0400

    Update BindDialKnobHandler.cpp

--- src/BindDialKnobHandler.cpp
+++ src/BindDialKnobHandler.cpp
@@ -1,9 +1,12 @@
 #include "BindDialKnobHandler.hpp"
-void DialKnobHandler::changed(int16_t val) {
-  if (value != NULL) {
+void DialKnobHandler::changed(int16_t val)
+{
+  if (value != NULL)
+  {
     *value = val;
   }
-  if (changeCallback != NULL) {
-	  changeCallback(val);
+  if (changeCallback != NULL)
+  {
+    changeCallback(val);
   }
 }
