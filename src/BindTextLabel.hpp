154abd98984a1f49fc417be399bdaaa50c847186
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-19 00:24:21 -0400

    Update BindTextLabel.hpp

--- src/BindTextLabel.hpp
+++ src/BindTextLabel.hpp
@@ -3,33 +3,74 @@
 #include "BindViewAutoTag.hpp"
 #include "BindUtils.hpp"
 // TODO: Extract the class to a serprate .h and .c file! Now!
-// TODO: Add the class summaries!
+/**
+ * @brief The BindTextLabel class represents a text label UI element for use with BindCanvas.
+ *
+ * BindTextLabel is a class that defines a text label UI element that can be used within BindCanvas-based
+ * applications. It inherits from the BindViewAutoTag class and provides functionality to create and
+ * customize text labels, including setting labels, positions, font size, and text color.
+ *
+ * Text labels created with BindTextLabel can be synchronized with BindCanvas through the Bind framework,
+ * allowing dynamic text content to be displayed in various applications.
+ *
+ * @note To use BindTextLabel effectively, you can set various properties such as the label, position,
+ * font size, and text color.
+ */
 class BindTextLabel : public BindViewAutoTag
 {
-private:
-  uint8_t objID = BindIDs::label;
-  uint16_t offset = 0;
-  int strLength = 0;
-  const char *str;
 
 public:
+  /**
+   * @brief Constructs a BindTextLabel with a custom label.
+   *
+   * This constructor creates a BindTextLabel with a custom text label provided as 'cstr.'
+   * The label can be used to define the text content of the label.
+   *
+   * @param cstr A pointer to the character array representing the custom label for the text label.
+   */
   BindTextLabel(const char *cstr)
   {
     setlabel(cstr);
   }
+
+  /**
+   * @brief Constructs a BindTextLabel with a default label.
+   *
+   * This constructor creates a BindTextLabel with a default label of "TextLabel."
+   * You can customize the label later using the 'setLabel' function.
+   */
   BindTextLabel()
   {
     setlabel("TextLabel");
   }
+
+  /**
+   * @brief Sets the label text for the text label.
+   *
+   * This function allows you to set the text label for the text label UI element, providing the content
+   * to be displayed.
+   *
+   * @param cstr A pointer to the character array representing the label text.
+   */
   void setlabel(const char *cstr)
   {
     str = cstr;
   }
-  int16_t x;
-  int16_t y;
-  uint8_t cmdId = 0;
-  int16_t fontSize;
-  int32_t color;
+
+  int16_t x;          ///< X-coordinate position of the text label
+  int16_t y;          ///< Y-coordinate position of the text label
+  uint8_t cmdId = 0;  ///< Command ID for the text label
+  int16_t fontSize;   ///< Font size for the text label
+  int32_t color;      ///< Text color for the text label
+
+  /**
+   * @brief Retrieves the bytes representing the text label for synchronization.
+   *
+   * This function is used to gather the bytes that represent the text label's properties for synchronization.
+   *
+   * @param out A pointer to the output buffer where the text label data will be stored.
+   * @return The number of bytes written to the output buffer.
+   */
   uint16_t getBytes(uint8_t *out) override
   {
     offset = 0;
@@ -48,6 +89,12 @@ public:
     copyAndOffset(out, &offset, str, strLength);
     return offset;
   }
+
+private:
+  uint8_t objID = BindIDs::label;
+  uint16_t offset = 0;
+  int strLength = 0;
+  const char *str;
 };
 
 #endif /* __BINDTEXTLABEL_H */
