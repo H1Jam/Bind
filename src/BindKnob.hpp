349a069f91ee5d94cd3c6d7f1c360e02dabc7319
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-19 00:24:17 -0400

    Update BindKnob.hpp

--- src/BindKnob.hpp
+++ src/BindKnob.hpp
@@ -3,35 +3,78 @@
 #include "BindView.h"
 #include "BindUtils.hpp"
 // TODO: Extract the class to a serprate .h and .c file! Now!
-// TODO: Add the class summaries!
+/**
+ * @brief The BindKnob class represents a knob UI element for use with BindCanvas.
+ *
+ * BindKnob is a class that defines a knob UI element that can be used within BindCanvas-based
+ * applications. It inherits from the BindView class and provides functionality to create and
+ * customize knobs, including setting labels, positions, dimensions, value ranges, and current values.
+ *
+ * Knobs created with BindKnob can be synchronized with BindCanvas through the Bind framework,
+ * allowing interactive user interfaces to be built for various applications.
+ *
+ * @note To use BindKnob effectively, you can set various properties such as the label, position,
+ * dimensions, value range (minimum and maximum), and the current knob value.
+ */
 class BindKnob : public BindView
 {
-private:
-  uint8_t objID = BindIDs::knob;
-  uint16_t offset = 0;
-  int strLength = 0;
-  const char *str;
 
 public:
+  /**
+   * @brief Constructs a BindKnob with a custom label.
+   *
+   * This constructor creates a BindKnob with a custom text label.
+   * The label can be used to define the knob's label or description.
+   *
+   * @param cstr A pointer to the character array representing the custom label for the knob.
+   */
   BindKnob(const char *cstr)
   {
     setlabel(cstr);
   }
+
+  /**
+   * @brief Constructs a BindKnob with a default label.
+   *
+   * This constructor creates a BindKnob with a default label of "Knob."
+   * You can customize the label later using the 'setLabel' function.
+   */
   BindKnob()
   {
     setlabel("Knob");
   }
-  int16_t x;
-  int16_t y;
-  uint8_t cmdId = 0;
-  int16_t dimSize;
-  int16_t minValue;
-  int16_t maxValue;
-  int16_t value;
+
+  int16_t x;         // X-coordinate position of the knob
+  int16_t y;         // Y-coordinate position of the knob
+  uint8_t cmdId = 0; // Command ID for the knob
+  int16_t dimSize;   // Dimensions or size of the knob
+  int16_t minValue;  // Minimum value of the knob's range
+  int16_t maxValue;  // Maximum value of the knob's range
+  int16_t value;     // Current value of the knob
+
+  /**
+   * @brief Sets the label text for the knob.
+   *
+   * This function allows you to set the text label for the knob, providing additional information
+   * or description for the knob's purpose.
+   * @note You need to sync the knob, e.g. call bind.sync(bindKnob).
+   *
+   * @param cstr A pointer to the character array representing the label text.
+   */
   void setlabel(const char *cstr)
   {
     str = cstr;
   }
+
+  /**
+   * @brief Retrieves the bytes representing the knob for synchronization.
+   *
+   * This function is used to gather the bytes that represent the knob's properties for synchronization.
+   * @note The user does not need to call this function, Bind calls it internally.
+   *
+   * @param out A pointer to the output buffer where the knob data will be stored.
+   * @return The number of bytes written to the output buffer.
+   */
   uint16_t getBytes(uint8_t *out) override
   {
     offset = 0;
@@ -52,5 +95,11 @@ public:
     copyAndOffset(out, &offset, str, strLength);
     return offset;
   }
+
+private:
+  uint8_t objID = BindIDs::knob;
+  uint16_t offset = 0;
+  int strLength = 0;
+  const char *str;
 };
 #endif /* __BINDKNOB_H */
