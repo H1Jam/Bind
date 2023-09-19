1b6c17e7d81bcdb64637122685159360b86e8e36
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-19 00:24:12 -0400

    Update BindButton.hpp

--- src/BindButton.hpp
+++ src/BindButton.hpp
@@ -3,34 +3,71 @@
 #include "BindView.h"
 #include "BindUtils.hpp"
 // TODO: Extract the class to a serprate .h and .c file! Now!
-// TODO: Add the class summaries!
+/**
+ * @brief The BindButton class represents a button UI element for use with BindCanvas.
+ *
+ * BindButton is a class that defines a button UI element that can be used within BindCanvas-based
+ * applications. It inherits from the BindView class and provides functionality to create and
+ * customize buttons, including setting labels, positions, colors, and sizes.
+ *
+ * Buttons created with BindButton can be synchronized with BindCanvas through the Bind framework,
+ * allowing interactive user interfaces to be built for various applications.
+ *
+ * @note To use BindButton effectively, you can set the label, position, font size, text color,
+ * background color, and other properties to tailor the button's appearance and behavior.
+ */
 class BindButton : public BindView
 {
-private:
-  uint8_t objID = BindIDs::button;
-  uint16_t offset = 0;
-  int strLength = 0;
-  const char *str;
 
 public:
+
+  /**
+   * @brief Constructs a BindButton with a custom label.
+   *
+   * This constructor creates a BindButton with a custom text label.
+   * The label can be used to define the button's text content.
+   *
+   * @param cstr A pointer to the character array representing the custom label for the button.
+   */
   BindButton(const char *cstr)
   {
     setlabel(cstr);
   }
+
   BindButton()
   {
     setlabel("Button");
   }
-  int16_t x;
-  int16_t y;
-  uint8_t cmdId = 0;
-  int16_t fontSize;
-  int32_t color;
-  int32_t backColor;
+
+  int16_t x;          // X-coordinate position of the button
+  int16_t y;          // Y-coordinate position of the button
+  uint8_t cmdId = 0;  // Command ID for the button
+  int16_t fontSize;   // Font size of the button's label
+  int32_t color;      // Text color of the button
+  int32_t backColor;  // Background color of the button
+
+  /**
+   * @brief Sets the label text for the button.
+   *
+   * This function allows you to set the text label for the button.
+   * @note You need to sync the button, e.g. call bind.sync(bindButton).
+   *
+   * @param cstr A pointer to the character array representing the label text.
+   */
   void setlabel(const char *cstr)
   {
     str = cstr;
   }
+
+  /**
+   * @brief Retrieves the bytes representing the button for synchronization.
+   *
+   * This function is used to gather the bytes that represent the button's properties for synchronization.
+   * @note The user does not need to call this function, Bind calls it internally.
+   * 
+   * @param out A pointer to the output buffer where the button data will be stored.
+   * @return The number of bytes written to the output buffer.
+   */
   uint16_t getBytes(uint8_t *out) override
   {
     offset = 0;
@@ -50,5 +87,12 @@ public:
     copyAndOffset(out, &offset, str, strLength);
     return offset;
   }
+
+  private:
+  
+  uint8_t objID = BindIDs::button;
+  uint16_t offset = 0;
+  int strLength = 0;
+  const char *str;
 };
 #endif /* __BINDBUTTON_H */
