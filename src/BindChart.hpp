a96e41601d50715ab862c6a361cbcc1f25054f66
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-20 03:23:56 -0400

    Update BindChart.hpp

--- src/BindChart.hpp
+++ src/BindChart.hpp
@@ -2,24 +2,46 @@
 #define __BINDCHART_HPP
 #include "BindViewAutoTag.hpp"
 #include "BindUtils.hpp"
+
+/**
+ * @brief Represents a customizable chart element for BindCanvas.
+ *
+ * The `BindChart` class provides the capability to create and display customizable charts within a BindCanvas interface.
+ * Users can define various attributes of the chart, such as its position, size, color, data ranges, and more.
+ * Additionally, users can dynamically update the chart data using the `bind.sync([float chartData], &myChart)` function.
+ *
+ * Example usage:
+ * - Create a `BindChart` object to represent the chart element.
+ * - Configure the chart's position, dimensions, color, and data ranges.
+ * - Optionally, set the chart to automatically adjust its size based on data.
+ * - Synchronize the chart with the BindCanvas using `bind.sync(&myChart)`.
+ * - Update the chart's data in real-time using the `bind.sync(136.36f, &myChart)` function.
+ *
+ */
 class BindChart : public BindViewAutoTag
 {
-private:
-    uint8_t objID = BindIDs::chart;
-    uint8_t dataID = BindIDs::chartData;
-    uint16_t offset = 0;
 
 public:
-    int16_t x = 0;
-    int16_t y = 0;
-    uint8_t cmdId = 0;
-    int16_t width = 200;
-    int16_t height = 100;
-    int16_t maxY = 10;
-    int16_t minY = -10;
-    int16_t maxX = 20;
-    bool autoSize = true;
-    int32_t color = YELLOW;
+    int16_t x = 0; ///< X-coordinate position of the chart.
+    int16_t y = 0; ///< Y-coordinate position of the chart.
+    uint8_t cmdId = 0; ///< Command ID for the chart.
+    int16_t width = 200; ///< Width of the chart.
+    int16_t height = 100; ///< Height of the chart.
+    int16_t maxY = 10; ///< Maximum Y-axis value of the chart.
+    int16_t minY = -10; ///< Minimum Y-axis value of the chart.
+    int16_t maxX = 20; ///< Maximum X-axis value of the chart.
+    bool autoSize = true; ///< Flag to enable auto-sizing based on data.
+    int32_t color = YELLOW; ///< Color of the chart.
+
+    /**
+     * @brief Generates and returns the byte data representing the chart configuration.
+     *
+     * This function is meant for internal use by the Bind framework and should not be called directly by users.
+     * It serializes the chart configuration properties into a byte array for communication with BindCanvas.
+     *
+     * @param out Pointer to the output byte array.
+     * @return The number of bytes written to the output array.
+     */
     uint16_t getBytes(uint8_t *out) override
     {
         offset = 0;
@@ -37,6 +59,17 @@ public:
         copyAndOffset(out, &offset, &color, sizeof(color));
         return offset;
     }
+
+    /**
+     * @brief Generates and returns the byte data representing chart data.
+     *
+     * This function is meant for internal use by the Bind framework and should not be called directly by users.
+     * It serializes the chart data properties into a byte array for communication with BindCanvas.
+     *
+     * @param out Pointer to the output byte array.
+     * @param chartData The data to be added to the chart.
+     * @return The number of bytes written to the output array.
+     */
     uint16_t getDataBytes(uint8_t *out, float chartData)
     {
         offset = 0;
@@ -47,6 +80,11 @@ public:
         copyAndOffset(out, &offset, &color, sizeof(color));
         return offset;
     }
+
+    private:
+    uint8_t objID = BindIDs::chart;
+    uint8_t dataID = BindIDs::chartData;
+    uint16_t offset = 0;
 };
 
 #endif /* __BINDCHART_HPP */
