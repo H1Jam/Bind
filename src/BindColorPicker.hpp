25e8a8abf41ace151e4f9a447b09a427a5de0704
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-21 02:55:02 -0400

    Update BindColorPicker.hpp

--- src/BindColorPicker.hpp
+++ src/BindColorPicker.hpp
@@ -6,17 +6,17 @@
 /**
  * @brief BindColorPicker Class
  *
- * The `BindColorPicker` class is part of the Bind framework for Arduino, designed to facilitate 
- * the creation of interactive user interface elements. Specifically, it represents a color picker 
+ * The `BindColorPicker` class is part of the Bind framework for Arduino, designed to facilitate
+ * the creation of interactive user interface elements. Specifically, it represents a color picker
  * element that can be displayed and controlled on the BindCanvas Android app interface.
  *
- * This class allows you to define a color picker's properties, including its position, size, and 
- * the initial RGB color values. You can synchronize these properties with BindCanvas to provide a 
+ * This class allows you to define a color picker's properties, including its position, size, and
+ * the initial RGB color values. You can synchronize these properties with BindCanvas to provide a
  * visual color selection component in your Arduino projects.
  *
- * To use the `BindColorPicker` class effectively, you can set its attributes such as the 
+ * To use the `BindColorPicker` class effectively, you can set its attributes such as the
  * position (x and y coordinates), dimensions (size), and initial color (red, green, and blue values).
- * The `cmdId` parameter specifies whether to add the color picker to the BindCanvas or refresh 
+ * The `cmdId` parameter specifies whether to add the color picker to the BindCanvas or refresh
  * an existing one.
  *
  * Additionally, you should regularly call `bind.sync()` in your main loop to ensure that the color picker
@@ -27,8 +27,8 @@
  *      BindColorPicker colorPicker;
  * - in your Setup() function:
  * bind.join(&colorPicker, &colorPicker_changed);
- * - colorPicker_changed is callback function like this: 
- * void colorPicker_changed(uint8_t red, uint8_t green, uint8_t blue) 
+ * - colorPicker_changed is callback function like this:
+ * void colorPicker_changed(uint8_t red, uint8_t green, uint8_t blue)
  * - and in the onConnection() callback:
  * colorPicker.x = 30;
  * colorPicker.y = 100;
@@ -45,13 +45,13 @@ class BindColorPicker : public BindView
 {
 
 public:
-    int16_t x = 0; ///< The x-coordinate position of the color picker on the screen.
-    int16_t y = 0; ///< The y-coordinate position of the color picker on the screen.
-    uint8_t cmdId = 0; ///< Command identifier to add or refresh the color picker.
+    int16_t x = 0;         ///< The x-coordinate position of the color picker on the screen.
+    int16_t y = 0;         ///< The y-coordinate position of the color picker on the screen.
+    uint8_t cmdId = 0;     ///< Command identifier to add or refresh the color picker.
     int16_t dimSize = 200; ///< The dimensions (size) of the color picker.
-    uint8_t red = 0; ///< The initial value for the red component of the selected color (0-255).
-    uint8_t green = 0; ///< The initial value for the green component of the selected color (0-255).
-    uint8_t blue = 0; ///< The initial value for the blue component of the selected color (0-255).
+    uint8_t red = 0;       ///< The initial value for the red component of the selected color (0-255).
+    uint8_t green = 0;     ///< The initial value for the green component of the selected color (0-255).
+    uint8_t blue = 0;      ///< The initial value for the blue component of the selected color (0-255).
 
     uint16_t getBytes(uint8_t *out) override
     {
@@ -67,6 +67,10 @@ public:
         copyAndOffset(out, &offset, &blue, sizeof(blue));
         return offset;
     }
+
+private:
+    uint8_t objID = BindIDs::colorPicker;
+    uint16_t offset = 0;
 };
 
 #endif /* __BINDCOLORPICKER_HPP */
