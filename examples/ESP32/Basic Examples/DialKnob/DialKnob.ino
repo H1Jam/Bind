301fc455787c95e06a25579f23a0c19e778555c2
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-22 19:25:30 -0400

    Update DialKnob.ino

--- examples/ESP32/Basic Examples/DialKnob/DialKnob.ino
+++ examples/ESP32/Basic Examples/DialKnob/DialKnob.ino
@@ -2,41 +2,22 @@
 #include "Bind.hpp"
 
 BluetoothSerial SerialBT;
-ScreenObjects screenObjects;
-ScreenKnob screenknob1;
+Bind bind;
+BindKnob knob1;
+BindKnob knob2;
 const int ledPin = 2;
-
-void screenDialKnobChanged(int16_t val)
-{
-  Serial.print("DialKnob has been changed:");
-  Serial.println(val);
-}
-
-void addKnob()
-{
-  screenknob1.x = 20;
-  screenknob1.y = 40;
-  screenknob1.cmdId = ADD_OR_REFRESH_CMD;
-  screenknob1.dimSize = 150;
-  screenknob1.minValue = -500;
-  screenknob1.maxValue = 500;
-  screenknob1.value = 200;
-  screenknob1.setlabel("Brightness");
-  sendScreenStream(&screenknob1, &SerialBT);
-}
-
-void screenSetup()
-{
-  Serial.println("Screen setup started!");
-  addKnob();
-  Serial.println("Screen setup done!");
-}
 
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
-  screenObjects.registerScreenSetup(&screenSetup);
-  screenObjects.registerDialKnob(&screenknob1, &screenDialKnobChanged);
+
+  // Initialize the Bind object and specify the communication method (SerialBT) and callback function (onConnection).
+  bind.init(&SerialBT, &onConnection);
+
+  // Connect the callback functions with the Bind objects.
+  bind.join(&knob1, &knob1_changed);
+  bind.join(&knob2, &knob2_changed);
+
   String devName = "ESP32testB";
   SerialBT.begin(devName);
   Serial.println("The bluetooth device started, now you can pair the phone with bluetooth!");
@@ -45,9 +26,60 @@ void setup() {
 }
 
 void loop() {
-  while (SerialBT.available()) {
-    screenObjects.updateScreen(SerialBT.read());
-  }
+  // Synchronize Bind UI events
+  bind.sync();
+
+  // Do some work...
   delay(10);
 }
 
+// Callback for Knob 1 Value Change
+void knob1_changed(int16_t val) {
+  Serial.print("Knob 1 value has changed to: ");
+  Serial.println(val);
+
+  // Some action:
+  if (val > 50) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+// Callback for Knob 2 Value Change
+void knob2_changed(int16_t val) {
+  Serial.print("Knob 2 has been changed to: ");
+  Serial.println(val);
+
+  // Implement your custom actions here based on the knob value
+}
+
+// Screen Setup Callback for BindCanvas
+void onConnection(int16_t w, int16_t h) {
+  Serial.println("Screen setup started!");
+  addKnobs();
+  Serial.println("Screen setup done!");
+}
+
+// Function to add knobs to the BindCanvas screen
+void addKnobs() {
+  // Configure and synchronize Knob 1
+  knob1.x = 100;
+  knob1.y = 100;
+  knob1.dimSize = 150; // The dimensions(width = height) of the knob1.
+  knob1.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  knob1.minValue = 0;
+  knob1.maxValue = 100;
+  knob1.value = 50;  // Initial value
+  bind.sync(&knob1);
+
+  // Configure and synchronize Knob 2
+  knob2.x = 100;
+  knob2.y = 260;
+  knob2.dimSize = 150; // The dimensions(width = height) of the knob2.
+  knob2.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  knob2.minValue = 0;
+  knob2.maxValue = 100;
+  knob2.value = 25;  // Initial value
+  bind.sync(&knob2);
+}
