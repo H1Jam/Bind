4c9c53db53e3557b8abb9b4f251423efe9a5541e
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-22 18:52:38 -0400

    Update Chart.ino

--- examples/ESP32/Basic Examples/Chart/Chart.ino
+++ examples/ESP32/Basic Examples/Chart/Chart.ino
@@ -7,17 +7,30 @@ BindChart chart1;
 
 int counter = 0;
 int X = 0;
-int sinX = 0;
+float sinX = 0;
 
-void sendDummySinWaveChartdata() {
+/**
+ * @brief Sends Dummy Sine Wave Chart Data
+ *
+ * This function generates a dummy sine wave data point and sends it to a BindChart object.
+ * The data point is synchronized with a BindChart object using the 'bind.sync' function.
+ */
+void send_Dummy_SinWave_Chartdata() {
   X += 10;
   X %= 360;
-  sinX = sin(sinX * PI / 180.0f) * 30 bind.sync(sinX, &chart1);
+  sinX = sin(X * PI / 180.0f) * 30;
+  bind.sync(sinX, &chart1);
 }
 
+/**
+ * @brief Adds a Chart to the BindCanvas
+ *
+ * This function configures and adds (or refreshes if already exists) a chart object to the BindCanvas screen.
+ * After configuring the chart, it is synchronized with the BindCanvas using 'bind.sync'.
+ */
 void addChart() {
-  chart1.x = 10;
-  chart1.y = 10;
+  chart1.x = 30;
+  chart1.y = 70;
   chart1.cmdId = BIND_ADD_OR_REFRESH_CMD;
   chart1.width = 300;
   chart1.height = 140;
@@ -29,6 +42,9 @@ void addChart() {
   bind.sync(&chart1);
 }
 
+/**
+ * @brief Screen Setup Callback for BindCanvas.
+ */
 void onConnection(int16_t w, int16_t h) {
   Serial.println("Screen setup started!");
   Serial.print(w);
@@ -56,6 +72,6 @@ void loop() {
   counter++;
   if (counter > 50) {
     counter = 0;
-    sendDummyChartdata();
+    send_Dummy_SinWave_Chartdata(); // Send sin wave data to update the chart
   }
 }
