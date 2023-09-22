ea4d52dbe020a0136b9e7132f53a52ce8993af63
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-22 17:08:15 -0400

    Update Chart.ino

--- examples/ESP32/Basic Examples/Chart/Chart.ino
+++ examples/ESP32/Basic Examples/Chart/Chart.ino
@@ -2,33 +2,35 @@
 #include "Bind.hpp"
 
 BluetoothSerial SerialBT;
-ScreenObjects screenObjects;
-ScreenChart screenChart;
+Bind bind;
+BindChart chart1;
 
 int counter = 0;
-int dummyData = 0;
+int X = 0;
+int sinX = 0;
 
-void sendDummyChartdata() {
-  dummyData += 10;
-  dummyData %= 360;
-  addChartdata(cos(dummyData*PI/180.0f)*30, &screenChart, &SerialBT);
+void sendDummySinWaveChartdata() {
+  X += 10;
+  X %= 360;
+  sinX = sin(sinX*PI/180.0f)*30
+  bind.sync(sinX, &chart1);
 }
 
 void addChart() {
-  screenChart.x = 10;
-  screenChart.y = 10;
-  screenChart.cmdId = ADD_OR_REFRESH_CMD;
-  screenChart.width = 300;
-  screenChart.height = 140;
-  screenChart.maxY = 10;
-  screenChart.minY = -10;
-  screenChart.maxX = 20;
-  screenChart.autoSize = true;
-  screenChart.color = YELLOW;
-  sendScreenStream(&screenChart, &SerialBT);
+  chart1.x = 10;
+  chart1.y = 10;
+  chart1.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  chart1.width = 300;
+  chart1.height = 140;
+  chart1.maxY = 10;
+  chart1.minY = -10;
+  chart1.maxX = 20;
+  chart1.autoSize = true;
+  chart1.color = YELLOW;
+  bind.sync(&chart1);
 }
 
-void screenSetup(int16_t w, int16_t h) {
+void onConnection(int16_t w, int16_t h) {
   Serial.println("Screen setup started!");
   Serial.print(w);
   Serial.print(" ");
@@ -39,7 +41,9 @@ void screenSetup(int16_t w, int16_t h) {
 
 void setup() {
   Serial.begin(115200);
-  screenObjects.registerScreenSetup(&screenSetup);
+  // Initialize the Bind object and specify the communication method (SerialBT) and callback function (onConnection).
+  bind.init(&SerialBT, &onConnection);
+
   String devName = "ESP32testB";
   SerialBT.begin(devName);
   Serial.println("The bluetooth device started, now you can pair the phone with bluetooth!");
