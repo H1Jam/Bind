441fb4dd93a4e2e54debde43a40d83082283aa80
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-22 15:17:44 -0400

    Update Button.ino

--- examples/ESP32/Basic Examples/Button/Button.ino
+++ examples/ESP32/Basic Examples/Button/Button.ino
@@ -7,27 +7,43 @@ ScreenButton screenButton1;
 const int ledPin = 2;
 bool ledIsON = false;
 
-void buttonClicked1()
-{
+void buttonClicked1() {
   Serial.println("button1 has been clicked!");
   ledIsON = !ledIsON;
   digitalWrite(ledPin, ledIsON);
 }
 
 void addButton1() {
-  screenButton1.x = 60; // location on android screen
-  screenButton1.y = 120; // location on android screen
-  screenButton1.color = BLACK; // Text color
-  screenButton1.backColor = YELLOW; // button color
-  screenButton1.setlabel("Button1"); // button label
-  screenButton1.fontSize = 23; // button label font size
+  // Syncing Button 1:
+  // Set the Button's position on the screen.
+  // Tip: You can use the grid view mode in BindCanvas app to determine the x and y coordinates
+  // and replace these numbers with the grid values for precise positioning.
+  button1.x = 30;
+  button1.y = 150;
+  // Set the Button's text label.
+  button1.setlabel("Click Me");  // button label
+  button1.fontSize = 23;         // The dimensions(width and height) of the Button on the screen are determined relative to the Font size.
+  button1.textColor = BLACK;         // Text color
+  button1.backColor = YELLOW;    // button color
+  // Specify the command to either add the object to the BindCanvas or refresh the existing one.
+  button1.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  // Synchronize the button1 object with BindCanvas.
+  bind.sync(&button1);
+}
+
+void addButton1() {
+  screenButton1.x = 60;               // location on android screen
+  screenButton1.y = 120;              // location on android screen
+  screenButton1.color = BLACK;        // Text color
+  screenButton1.backColor = YELLOW;   // button color
+  screenButton1.setlabel("Button1");  // button label
+  screenButton1.fontSize = 23;        // button label font size
   screenButton1.cmdId = ADD_OR_REFRESH_CMD;
-  
+
   sendScreenStream(&screenButton1, &SerialBT);
 }
 
-void screenSetup(int16_t w, int16_t h)
-{
+void screenSetup(int16_t w, int16_t h) {
   Serial.println("Screen setup started!");
   addButton1();
   Serial.println("Screen setup done!");
@@ -51,4 +67,3 @@ void loop() {
   }
   delay(10);
 }
-
