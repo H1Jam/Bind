c04aa660c608b10b72827272b971245df72f7bb6
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-23 03:48:53 -0400

    Update DrawBox.ino

--- examples/ESP32/Basic Examples/DrawBox/DrawBox.ino
+++ examples/ESP32/Basic Examples/DrawBox/DrawBox.ino
@@ -6,85 +6,54 @@ Bind bind;
 BindRectangle rectangle1;
 BindRectangle rectangle2;
 
-// void addBoxes() {
-//   screenRectangle1.x = 10;
-//   screenRectangle1.y = 10;
-//   screenRectangle1.cmdId = ADD_OR_REFRESH_CMD;
-//   screenRectangle1.width = 300;
-//   screenRectangle1.height = 100;
-//   screenRectangle1.fillColor = UBUNTU;
-//   screenRectangle1.strokeColor = YELLOW;
-//   screenRectangle1.strokeWidth = 2;
-//   screenRectangle1.cornersRadius = 5;
-//   sendScreenStream(&screenRectangle1, &SerialBT);
-  
-//   screenRectangle2.x = 10;
-//   screenRectangle2.y = 120;
-//   screenRectangle2.cmdId = ADD_OR_REFRESH_CMD;
-//   screenRectangle2.width = 300;
-//   screenRectangle2.height = 200;
-//   screenRectangle2.fillColor = UBUNTU;
-//   screenRectangle2.strokeColor = YELLOW;
-//   screenRectangle2.strokeWidth = 2;
-//   screenRectangle2.cornersRadius = 10;
-//   sendScreenStream(&screenRectangle2, &SerialBT);
-  
-//   screenRectangle3.x = 20;
-//   screenRectangle3.y = 260;
-//   screenRectangle3.cmdId = ADD_OR_REFRESH_CMD;
-//   screenRectangle3.width = 280;
-//   screenRectangle3.height = 50;
-//   screenRectangle3.fillColor = BLACK;
-//   screenRectangle3.strokeColor = YELLOW;
-//   screenRectangle3.strokeWidth = 2;
-//   screenRectangle3.cornersRadius = 50;
-//   sendScreenStream(&screenRectangle3, &SerialBT);
-// }
-
 // onConnection Callback for BindCanvas
-void onConnection(int16_t w, int16_t h) {
+void onConnection(int16_t width, int16_t height) {
   Serial.println("Screen setup started!");
   configureCanvasSettings();
-  addRectangles();
+  addRectangle();
+  drawAdaptiveRectangle(width);
   Serial.println("Screen setup done!");
 }
 
 // Function to configure the BindCanvas screen settings
 void configureCanvasSettings() {
   // Set the screen orientation (0 for portrait, 1 for landscape)
+  BindCanvasSettings canvasSettings;
   canvasSettings.screenOrientation = SCREEN_ORIENTATION_PORTRAIT;
   // Set the background color of the screen (in RGB format)
-  canvasSettings.backColor = WHITE;
+  canvasSettings.backColor = RGB(240, 240, 240);;
   // Synchronize the canvasSettings object with BindCanvas
   bind.sync(&canvasSettings);
 }
 
 // Function to add a rectangle to the BindCanvas screen
-void addRectangles() {
+void addRectangle() {
   // Configure and synchronize the rectangle
   rectangle1.x = 10;
-  rectangle1.y = 70;
+  rectangle1.y = 280;
   rectangle1.width = 200;
-  rectangle1.height = 100;
+  rectangle1.height = 200;
   rectangle1.cmdId = BIND_ADD_OR_REFRESH_CMD;
   rectangle1.fillColor = WHITE; // Fill color of the rectangle (in RGB format)
   rectangle1.borderColor = RGB(100, 100, 100); // Border color of the rectangle (in RGB format) 
-  rectangle1.borderWidth = 2; // Border width in pixels
+  rectangle1.borderWidth = 1; // Border width
   rectangle1.cornersRadius = 5;  /// Radius of the rectangle's rounded corners.
   bind.sync(&rectangle1);
+}
 
+void drawAdaptiveRectangle(int16_t screenWidth){
   rectangle2.x = 10;
-  rectangle2.y = 175;
-  rectangle2.width = 200;
+  rectangle2.y = 70;
+  rectangle2.width = screenWidth - rectangle2.x*2;
   rectangle2.height = 200;
   rectangle2.cmdId = BIND_ADD_OR_REFRESH_CMD;
   rectangle2.fillColor = WHITE; // Fill color of the rectangle (in RGB format)
   rectangle2.borderColor = DKGRAY; // Border color of the rectangle (in RGB format) 
-  rectangle2.borderWidth = 2; // Border width in pixels
-  rectangle2.cornersRadius = 5;  /// Radius of the rectangle's rounded corners.
+  rectangle2.borderWidth = 0; // Border width
+  rectangle2.cornersRadius = 2;  /// Radius of the rectangle's rounded corners.
   bind.sync(&rectangle2);
-
 }
+
 
 void setup() {
   Serial.begin(115200);
