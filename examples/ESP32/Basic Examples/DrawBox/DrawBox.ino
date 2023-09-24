bf3b72f9a11cd4b9ecb44a786f05e0b2fa7d3da9
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-24 04:41:31 -0400

    Update DrawBox.ino

--- examples/ESP32/Basic Examples/DrawBox/DrawBox.ino
+++ examples/ESP32/Basic Examples/DrawBox/DrawBox.ino
@@ -21,7 +21,7 @@ void configureCanvasSettings() {
   BindCanvasSettings canvasSettings;
   canvasSettings.screenOrientation = SCREEN_ORIENTATION_PORTRAIT;
   // Set the background color of the screen (in RGB format)
-  canvasSettings.backColor = RGB(240, 240, 240);;
+  canvasSettings.backColor = RGB(240, 240, 240);
   // Synchronize the canvasSettings object with BindCanvas
   bind.sync(&canvasSettings);
 }
@@ -34,30 +34,30 @@ void addRectangle() {
   rectangle1.width = 200;
   rectangle1.height = 200;
   rectangle1.cmdId = BIND_ADD_OR_REFRESH_CMD;
-  rectangle1.fillColor = WHITE; // Fill color of the rectangle (in RGB format)
-  rectangle1.borderColor = RGB(100, 100, 100); // Border color of the rectangle (in RGB format) 
-  rectangle1.borderWidth = 1; // Border width
-  rectangle1.cornersRadius = 5;  /// Radius of the rectangle's rounded corners.
+  rectangle1.fillColor = WHITE;                 // Fill color of the rectangle (in RGB format)
+  rectangle1.borderColor = RGB(100, 100, 100);  // Border color of the rectangle (in RGB format)
+  rectangle1.borderWidth = 1;                   // Border width
+  rectangle1.cornersRadius = 5;                 /// Radius of the rectangle's rounded corners.
   bind.sync(&rectangle1);
 }
 
-void drawAdaptiveRectangle(int16_t screenWidth){
+void drawAdaptiveRectangle(int16_t screenWidth) {
   rectangle2.x = 10;
   rectangle2.y = 70;
-  rectangle2.width = screenWidth - rectangle2.x*2;
+  rectangle2.width = screenWidth - rectangle2.x * 2;
   rectangle2.height = 200;
   rectangle2.cmdId = BIND_ADD_OR_REFRESH_CMD;
-  rectangle2.fillColor = WHITE; // Fill color of the rectangle (in RGB format)
-  rectangle2.borderColor = DKGRAY; // Border color of the rectangle (in RGB format) 
-  rectangle2.borderWidth = 0; // Border width
-  rectangle2.cornersRadius = 2;  /// Radius of the rectangle's rounded corners.
+  rectangle2.fillColor = WHITE;     // Fill color of the rectangle (in RGB format)
+  rectangle2.borderColor = DKGRAY;  // Border color of the rectangle (in RGB format)
+  rectangle2.borderWidth = 0;       // Border width
+  rectangle2.cornersRadius = 2;     /// Radius of the rectangle's rounded corners.
   bind.sync(&rectangle2);
 }
 
 
 void setup() {
   Serial.begin(115200);
-    // Initialize the Bind object and specify the communication method (SerialBT) and callback function (onConnection).
+  // Initialize the Bind object and specify the communication method (SerialBT) and callback function (onConnection).
   bind.init(&SerialBT, &onConnection);
   String devName = "BindingESP32";
   SerialBT.begin(devName);
@@ -73,4 +73,3 @@ void loop() {
   // Add a small delay to control the loop speed
   delay(10);
 }
-
