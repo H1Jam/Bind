bb95f3381c84e4bc848d7b4b9abceca6fa8524b9
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-25 02:08:25 -0400

    Update ScreenConfigs.ino

--- examples/ESP32/Basic Examples/ScreenConfigs/ScreenConfigs.ino
+++ examples/ESP32/Basic Examples/ScreenConfigs/ScreenConfigs.ino
@@ -2,37 +2,48 @@
 #include "Bind.hpp"
 
 BluetoothSerial SerialBT;
-ScreenObjects screenObjects;
-ScreenSettings screenSettings;
+Bind bind;
+BindCanvasSettings canvasSettings;
 
-void screenAppearance() {
-  screenSettings.screenOrientation = SCREEN_ORIENTATION_UNSPECIFIED;
-  screenSettings.backColor = UBUNTU;
-  screenSettings.actionBarColor = DKGRAY;
-  screenSettings.setlabel("Hello Android");
-  sendScreenStream(&screenSettings, &SerialBT);
+void setScreenConfig() {
+  // Configure screen settings
+
+  // Screen orientation is unspecified, allowing the system to decide.
+  //canvasSettings.screenOrientation = SCREEN_ORIENTATION_UNSPECIFIED;
+
+  // Portrait mode: screen height > width.
+  //canvasSettings.screenOrientation = SCREEN_ORIENTATION_PORTRAIT;
+
+  // Landscape mode: screen width > height.
+  canvasSettings.screenOrientation = SCREEN_ORIENTATION_LANDSCAPE;
+  canvasSettings.backColor = BLACK;
+  bind.sync(&canvasSettings);
 }
 
-void screenSetup()
-{
-  Serial.println("Screen setup started!");
-  screenAppearance();
+void onConnection(int16_t width, int16_t height) {
+  Serial.print("Screen setup started!");
+  Serial.print(width);
+  Serial.print("x");
+  Serial.println(height);
+
+  setScreenConfig();
+
   Serial.println("Screen setup done!");
 }
 
 void setup() {
   Serial.begin(115200);
-  screenObjects.registerScreenSetup(&screenSetup);
-  String devName = "ESP32testB";
+  String devName = "BindOnESP32";
   SerialBT.begin(devName);
+
+  bind.init(&SerialBT, &onConnection);
+
   Serial.println("The bluetooth device started, now you can pair the phone with bluetooth!");
   Serial.println("devName:");
   Serial.println(devName);
 }
 
 void loop() {
-  while (SerialBT.available()) {
-    screenObjects.updateScreen(SerialBT.read());
-  }
-  delay(10);
+  bind.sync();
+  delay(2);
 }
