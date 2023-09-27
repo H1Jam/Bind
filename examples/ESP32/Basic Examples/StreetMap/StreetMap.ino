921f8e5c1a3054faa3057306f26e1d2fac759f0c
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-09-27 01:31:29 -0400

    Update StreetMap.ino

--- examples/ESP32/Basic Examples/StreetMap/StreetMap.ino
+++ examples/ESP32/Basic Examples/StreetMap/StreetMap.ino
@@ -2,10 +2,10 @@
 #include "Bind.hpp"
 
 BluetoothSerial SerialBT;
-ScreenObjects screenObjects;
-ScreenMap screenMap;
-ScreenMapMarker screenMapMarker1;
-ScreenMapMarker screenMapMarker2;
+Bind bind;
+BindMap bindMap;
+BindMapMarker mapMarker1;
+BindMapMarker mapMarker2;
 
 int counter = 0;
 int wpIndex = 0;
@@ -17,50 +17,64 @@ float oceanDrive[5][2] = {
   { 26.890472, -80.056670 }
 };
 
+// Add the map on screen (You can only have one map on the screen for now.)
 void addMap() {
-  screenMap.cmdId = ADD_OR_REFRESH_CMD;
-  screenMap.x = 10;
-  screenMap.y = 10;
-  screenMap.width = 300;
-  screenMap.height = 400;
-  screenMap.lat = 26.891425f;
-  screenMap.lon = -80.056879f;
-  screenMap.mapOrientation = 0.0f;
-  screenMap.zoom = 18;
-  sendScreenStream(&screenMap, &SerialBT);
+  bindMap.x = 10;
+  bindMap.y = 70;
+  bindMap.width = 300;
+  bindMap.height = 400;
+  bindMap.lat = 26.891425f;
+  bindMap.lon = -80.056879f;
+  bindMap.mapOrientation = 0.0f;
+  bindMap.zoom = 18;
+  bindMap.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  bind.sync(&bindMap);
 }
 
+void moveMapAndMarkers() {
+  mapMarker1.lat = oceanDrive[wpIndex][0];
+  mapMarker1.lon = oceanDrive[wpIndex][1];
+  mapMarker1.rotation = 167.0f;
+  /* all the possible values for iconId: 
+    MARKER_PIN_RED
+    MARKER_PIN_GREEN
+    MARKER_PIN_BLUE
+    MARKER_PIN_YELLOW
+    MARKER_CAR_RED
+    MARKER_CAR_GREEN
+    MARKER_CAR_BLUE
+    MARKER_CAR_YELLOW
+    MARKER_PLANE_RED 8
+    MARKER_PLANE_GREEN
+    MARKER_PLANE_BLUE
+    MARKER_PLANE_YELLOW 
+  */
+  mapMarker1.iconId = MARKER_CAR_RED; 
+  mapMarker1.scale = 70;
+  mapMarker1.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  bind.sync(&mapMarker1);
 
-void setMapMarker() {
-  screenMapMarker1.lat = oceanDrive[wpIndex][0];
-  screenMapMarker1.lon = oceanDrive[wpIndex][1];
-  screenMapMarker1.rotation = 167.0f;
-  screenMapMarker1.iconId = MarkerIcons::CarRed;
-  screenMapMarker2.scale = 70;
-  screenMapMarker1.cmdId = ADD_OR_REFRESH_CMD;
-  sendScreenStream(&screenMapMarker1, &SerialBT);
+  mapMarker2.lat = oceanDrive[4 - wpIndex][0];
+  mapMarker2.lon = oceanDrive[4 - wpIndex][1];
+  mapMarker2.rotation = 345.0f;
+  mapMarker2.iconId = MARKER_CAR_GREEN;
+  mapMarker2.scale = 60;
+  mapMarker2.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  bind.sync(&mapMarker2);
 
-  screenMapMarker2.lat = oceanDrive[4 - wpIndex][0];
-  screenMapMarker2.lon = oceanDrive[4 - wpIndex][1];
-  screenMapMarker2.rotation = 345.0f;
-  screenMapMarker2.iconId = MarkerIcons::CarGreen;
-  screenMapMarker2.scale = 60;
-  screenMapMarker2.cmdId = ADD_OR_REFRESH_CMD;
-  sendScreenStream(&screenMapMarker2, &SerialBT);
-
-  screenMap.lat = oceanDrive[wpIndex][0];
-  screenMap.lon = oceanDrive[wpIndex][1];
-  screenMap.mapOrientation = 0.0f;
-  screenMap.zoom = 18;
-  sendScreenStream(&screenMap, &SerialBT);
+  bindMap.lat = oceanDrive[wpIndex][0];
+  bindMap.lon = oceanDrive[wpIndex][1];
+  bindMap.mapOrientation = 0.0f;
+  bindMap.zoom = 18;  // or BIND_MAP_USER_ZOOM to let the user control the zoom level.
+  bindMap.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  bind.sync(&bindMap);
   wpIndex++;
   if (wpIndex > 4) {
     wpIndex = 0;
   }
 }
 
-
-void screenSetup() {
+void onConnection(int16_t width, int16_t height) {
   Serial.println("Screen setup started!");
   addMap();
   Serial.println("Screen setup done!");
@@ -68,22 +82,24 @@ void screenSetup() {
 
 void setup() {
   Serial.begin(115200);
-  screenObjects.registerScreenSetup(&screenSetup);
-  String devName = "ESP32testB";
+  String devName = "BindOnESP32";
   SerialBT.begin(devName);
+
+  /// Initialize the Bind object and specify the communication method (SerialBT) and callback function (onConnection).
+  bind.init(&SerialBT, &onConnection);
+  /// Note: It was SerialBT here, but it could be any serial port, including hardware and software serial.
+
   Serial.println("The bluetooth device started, now you can pair the phone with bluetooth!");
   Serial.println("devName:");
   Serial.println(devName);
 }
 
 void loop() {
-  while (SerialBT.available()) {
-    screenObjects.updateScreen(SerialBT.read());
-  }
+  bind.sync();
   delay(100);
   counter++;
   if (counter > 10) {
     counter = 0;
-    setMapMarker();
+    moveMapAndMarkers();
   }
 }
