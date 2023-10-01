a1f50c5b8339fab1916bb2b685bf6c1c32acd4d0
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-10-01 04:24:30 -0400

    Create Button.ino

---
+++ examples/All Boards+Bluetooth Module/Basic Examples/Button/Button.ino
@@ -0,0 +1,119 @@
+#include <SoftwareSerial.h>
+#include "Bind.hpp"
+
+// Note: Adjust the pins to match your Bluetooth module's configuration.
+#ifdef __AVR__
+SoftwareSerial swSerial(4, 3);
+#else
+SoftwareSerial swSerial(D4, D3); // For boards like ESP8266, ESP32, or similar.
+#endif
+Bind bind;
+BindButton button1;
+BindButton button2;
+
+const int ledPin = 2;
+
+/**
+ * @brief Callback for Button 1 Press Event
+ *
+ * This function is automatically triggered when Button 1 is pressed on the screen.
+ * It allows you to define specific actions to perform when the button is pressed.
+ * To link this callback function with the Bind object, use the following syntax (in the Setup()):
+ * `bind.join(&your_button_object, &button1_pressed);`
+ * Make sure to include the '&' symbols as they indicate pointers to the function and object.
+ * @note Ensure you call "bind.sync();" in the main loop to receive button press events.
+ */
+void button1_pressed() {
+  Serial.println("Button 1 has been pressed!");
+
+  // Implement your custom actions here:
+  digitalWrite(ledPin, HIGH);
+
+  // Add more actions as needed.
+}
+
+/**
+ * @brief Callback for Button 1 Press Event
+ */
+void button2_pressed() {
+  // See button1_pressed descripotions!
+  Serial.println("Button 2 has been pressed!");
+  digitalWrite(ledPin, LOW);
+
+  // Add more actions as needed.
+}
+
+/**
+ * @brief Adds Button 1 to the BindCanvas
+ *
+ * This function adds (or refreshes, if already exist) Button 1 on the BindCanvas screen.
+ * It synchronizes the Button's properties with the BindCanvas to display it correctly.
+ * You can customize the Button's position, size, text, and more.
+ * @attention a button object need the calback function as well. See the button1_pressed()
+ * function.
+ * @note Ensure you call "bind.sync();" in the main loop to receive button press events.
+ */
+void addButton1() {
+  // Syncing Button 1:
+  // Set the Button's position on the screen.
+  // Tip: You can use the grid view mode in BindCanvas app to determine the x and y coordinates
+  // and replace these numbers with the grid values for precise positioning.
+  button1.x = 30;
+  button1.y = 150;
+  // Set the Button's text label.
+  button1.setlabel("ON");     // button label
+  button1.fontSize = 23;      // The dimensions(width and height) of the Button on the screen are determined relative to the Font size.
+  button1.textColor = BLACK;  // Text color
+  button1.backColor = GREEN;  // button color
+  // Specify the command to either add the object to the BindCanvas or refresh the existing one.
+  button1.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  // Synchronize the button1 object with BindCanvas.
+  bind.sync(&button1);
+}
+
+void addButton2() {
+  // Syncing Button 2:
+  // See addButton1 descripotions!
+  button2.x = 30;
+  button2.y = 200;
+  // Set the Button's text label.
+  button2.setlabel("OFF");     // button label
+  button2.fontSize = 23;       // The dimensions(width and height) of the Button on the screen are determined relative to the Font size.
+  button2.textColor = BLACK;   // Text color
+  button2.backColor = YELLOW;  // button color
+  // Specify the command to either add the object to the BindCanvas or refresh the existing one.
+  button2.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  // Synchronize the button1 object with BindCanvas.
+  bind.sync(&button2);
+}
+
+void onConnection(int16_t w, int16_t h) {
+  Serial.println("Screen setup started!");
+  addButton1();
+  addButton2();
+  Serial.println("Screen setup done!");
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  // Note: Adjust the baud rate to match your Bluetooth module's configuration.
+  swSerial.begin(57600);
+  // Initialize the Bind object and specify the communication method (swSerial) and callback function (onConnection).
+  bind.init(&swSerial, &onConnection);
+  // Note: It was swSerial here, but it could be any serial port, including hardware and software serial.
+
+  // Connect the callback functions with the Bind objects.
+  bind.join(&button1, &button1_pressed);
+  bind.join(&button2, &button2_pressed);
+
+}
+
+void loop() {
+  // Regularly synchronize Bind UI events to receive button press events.
+  bind.sync();
+
+  // This delay is not an essential part of the code
+  // but is included here to simulate the other work you may want to do.
+  delay(10);
+}
