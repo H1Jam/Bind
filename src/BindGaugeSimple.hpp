c586760b1fa5a1736969ba1fbc7966a00eb04a14
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-10-01 04:24:05 -0400

    Update BindGaugeSimple.hpp

--- src/BindGaugeSimple.hpp
+++ src/BindGaugeSimple.hpp
@@ -88,29 +88,7 @@ public:
      * @param out A pointer to the byte array where the serialized data will be stored.
      * @return The offset representing the length of the serialized data.
      */
-    uint16_t getBytes(uint8_t *out) override
-    {
-        offset = 0;
-        strLength = strlen(str);
-        if (strLength > MAX_STRING_LENGTH_TERMINAL)
-        {
-            strLength = MAX_STRING_LENGTH_TERMINAL;
-        }
-        copyAndOffset(out, &offset, &objID, sizeof(objID));
-        copyAndOffset(out, &offset, &x, sizeof(x));
-        copyAndOffset(out, &offset, &y, sizeof(y));
-        copyAndOffset(out, &offset, &tag, sizeof(tag));
-        copyAndOffset(out, &offset, &cmdId, sizeof(cmdId));
-        copyAndOffset(out, &offset, &dimSize, sizeof(dimSize));
-        copyAndOffset(out, &offset, &minValue, sizeof(minValue));
-        copyAndOffset(out, &offset, &maxValue, sizeof(maxValue));
-        copyAndOffset(out, &offset, &value, sizeof(value));
-        copyAndOffset(out, &offset, &style, sizeof(style));
-        copyAndOffset(out, &offset, &isSymmetrical, sizeof(isSymmetrical));
-        copyAndOffset(out, &offset, &color, sizeof(color));
-        copyAndOffset(out, &offset, str, strLength);
-        return offset;
-    }
+    uint16_t getBytes(uint8_t *out);
 
 private:
     uint8_t objID = BIND_ID_GAUGE_SIMPLE; ///< The unique identifier for the simple gauge object.
