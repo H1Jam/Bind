ed3dc3a1ad3e79853c303dc081f473f8c6572a55
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-10-01 03:57:15 -0400

    Update BindView.hpp

--- src/BindView.hpp
+++ src/BindView.hpp
@@ -2,23 +2,21 @@
 #define __BINDVIEW_HPP
 #include <stdint.h>
 
-static int32_t BLACK = 0xFF000000;
-static int32_t DKGRAY = 0xFF444444;
-static int32_t GRAY = 0xFF888888;
-static int32_t LTGRAY = 0xFFCCCCCC;
-static int32_t WHITE = 0xFFFFFFFF;
-static int32_t RED = 0xFFFF0000;
-static int32_t GREEN = 0xFF00FF00;
-static int32_t BLUE = 0xFF0000FF;
-static int32_t YELLOW = 0xFFFFFF00;
-static int32_t CYAN = 0xFF00FFFF;
-static int32_t MAGENTA = 0xFFFF00FF;
-static int32_t UBUNTU = 0xFF300A24;
-static int32_t AVOCADO = 0xFF95986B;
-static int32_t TRANSPARENT = 0;
-
-static uint8_t MAX_STRING_LENGTH = 32;
-static uint8_t MAX_STRING_LENGTH_TERMINAL = 64;
+#define BLACK 0xFF000000
+#define DKGRAY 0xFF444444
+#define GRAY 0xFF888888
+#define LTGRAY 0xFFCCCCCC
+#define WHITE 0xFFFFFFFF
+#define RED 0xFFFF0000
+#define GREEN 0xFF00FF00
+#define BLUE 0xFF0000FF
+#define YELLOW 0xFFFFFF00
+#define CYAN 0xFF00FFFF
+#define MAGENTA 0xFFFF00FF
+#define UBUNTU 0xFF300A24
+#define AVOCADO 0xFF95986B
+#define TRANSPARENT 0
+#define MAX_STRING_LENGTH_TERMINAL 64
 
 class BindView
 {
