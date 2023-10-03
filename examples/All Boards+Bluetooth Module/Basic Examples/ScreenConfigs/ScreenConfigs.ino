70e10869214c1ca1adf6bf2378e71d6c21e5119f
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-10-03 01:57:02 -0400

    Create ScreenConfigs.ino

---
+++ examples/All Boards+Bluetooth Module/Basic Examples/ScreenConfigs/ScreenConfigs.ino
@@ -0,0 +1,54 @@
+#include <SoftwareSerial.h>
+#include "Bind.hpp"
+
+// Note: Adjust the pins to match your Bluetooth module's configuration.
+#ifdef __AVR__
+SoftwareSerial swSerial(4, 3);
+#else
+SoftwareSerial swSerial(D4, D3); // For boards like ESP8266, ESP32, or similar.
+#endif
+Bind bind;
+BindCanvasSettings canvasSettings;
+
+void setScreenConfig() {
+  // Configure screen settings
+
+  // Screen orientation is unspecified, allowing the system to decide.
+  //canvasSettings.screenOrientation = SCREEN_ORIENTATION_UNSPECIFIED;
+
+  // Portrait mode: screen height > width.
+  //canvasSettings.screenOrientation = SCREEN_ORIENTATION_PORTRAIT;
+
+  // Landscape mode: screen width > height.
+  canvasSettings.screenOrientation = SCREEN_ORIENTATION_LANDSCAPE;
+  canvasSettings.backColor = BLACK; 
+  // Or use RGB(R:0-255, G:0-255, B0-255). 
+  //For example:
+  //canvasSettings.backColor = RGB(100,255,40);
+  bind.sync(&canvasSettings);
+}
+
+void onConnection(int16_t width, int16_t height) {
+  Serial.print("Screen setup started!");
+  Serial.print(width);
+  Serial.print("x");
+  Serial.println(height);
+
+  setScreenConfig();
+
+  Serial.println("Screen setup done!");
+}
+
+void setup() {
+  Serial.begin(115200);
+  // Note: Adjust the baud rate to match your Bluetooth module's configuration.
+  swSerial.begin(57600);
+
+  bind.init(&swSerial, &onConnection);
+
+}
+
+void loop() {
+  bind.sync();
+  delay(2);
+}
