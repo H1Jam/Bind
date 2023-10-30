c01a6ba7d3916fe6c8402df26ab0bb50910bdd60
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-10-30 02:06:49 -0400

    Update Terminal.ino

--- examples/USB/Basic Examples/Terminal/Terminal.ino
+++ examples/USB/Basic Examples/Terminal/Terminal.ino
@@ -1,44 +1,46 @@
 #include "Bind.hpp"
 
-ScreenObjects screenObjects;
-ScreenTerminal screenTerminal;
+Bind bind;
+BindTerminal bindTerminal;
 
 int counter = 0;
 int counter2 = 0;
 char buffer[15];
 
 void addScreenTerminal() {
-  screenTerminal.x = 10;
-  screenTerminal.y = 10;
-  screenTerminal.cmdId = ADD_OR_REFRESH_CMD;
-  screenTerminal.width = 300;
-  screenTerminal.height = 200;
-  screenTerminal.textSize = 10;
-  screenTerminal.backColor = UBUNTU;
-  sendScreenStream(screenTerminal, Serial);
+  bindTerminal.x = 10;
+  bindTerminal.y = 70;
+  bindTerminal.cmdId = BIND_ADD_OR_REFRESH_CMD;
+  bindTerminal.width = 300;
+  bindTerminal.height = 200;
+  bindTerminal.textSize = 10;
+  bindTerminal.backColor = UBUNTU;
+  bind.sync(bindTerminal);
 }
 
 void updateScreenTerminalData(const char *cstr) {
-  ScreenTerminalPrint(cstr, GREEN, true, true, true, false, screenTerminal, Serial);
+  bind.sync(cstr, GREEN, true, true, true, false, bindTerminal);
 }
 
 void updateScreenTerminalDataBigger(const char *cstr) {
-  ScreenTerminalPrint(cstr, WHITE, true, true, true, false, screenTerminal, Serial);
+  bind.sync(cstr, WHITE, true, true, true, false, bindTerminal);
 }
 
-void screenSetup() {
+void onConnection(int16_t w, int16_t h) {
   addScreenTerminal();
 }
 
 void setup() {
   Serial.begin(115200);
-  screenObjects.registerScreenSetup(screenSetup);
+  // Initialize the Bind object and specify the communication method (swSerial) and callback function (onConnection).
+  bind.init(Serial, onConnection);
+  // Note: We used Serial here, but it could be any serial port, including hardware and software serial.
 }
 
 void loop() {
-  while (Serial.available()) {
-    screenObjects.updateScreen(Serial.read());
-  }
+  // Regularly synchronize Bind UI events to receive events.
+  bind.sync();
+  
   delay(10);
   counter++;
   if (counter > 100) {
