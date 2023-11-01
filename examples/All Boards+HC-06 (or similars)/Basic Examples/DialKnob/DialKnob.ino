69202a849ddc50d249f65b6709967f82ef276f3d
H1Jam <46633362+H1Jam@users.noreply.github.com>
2023-11-01 00:39:37 -0400

    Update DialKnob.ino

--- examples/All Boards+HC-06 (or similars)/Basic Examples/DialKnob/DialKnob.ino
+++ examples/All Boards+HC-06 (or similars)/Basic Examples/DialKnob/DialKnob.ino
@@ -14,10 +14,15 @@ SoftwareSerial btSerial(D1, D2);  // For ESP8266: RX: pin D1, TX: pin D2
 #elif defined(ESP32)
 #define btSerial Serial2  // For ESP32 we use Serial2.
 #elif defined(ARDUINO_ARCH_RP2040)
-#define btSerial Serial1  // For RP2040(Raspberry Pi Pico): Use serial1 RX: pin 2, TX: pin 3
+#define btSerial Serial1  // For RP2040(Raspberry Pi Pico): Use serial1 RX: pin 2, TX: pin 1
 #else
 SoftwareSerial btSerial(4, 3);  // Modify this line, if your board is neither above.
 #endif
+
+// If the board doesn't have built-in LED, assign an external LED here (if wish to use it):
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 10
+#endif  
 
 Bind bind;
 BindKnob knob1;
