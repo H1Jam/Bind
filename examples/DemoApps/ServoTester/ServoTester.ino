3429a7d0899fe793041bd4feae21c7b3478131e2
H1Jam <46633362+H1Jam@users.noreply.github.com>
2024-02-25 23:18:17 -0500

    Update ServoTester.ino

--- examples/DemoApps/ServoTester/ServoTester.ino
+++ examples/DemoApps/ServoTester/ServoTester.ino
@@ -1,105 +1,108 @@
-// Atention: For Raspberry Pi Pico W, this library needs Bluetooth enabled.
-// Use the 'Tools->IP/Bluetooth Stack' menu in the IDE to enable it (x + Bluetooth).
-
-// Include libraries
-#ifdef __AVR__
-#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
-#endif
+/**
+ * @brief This is a demo application for testing the Bind library with a servo motor.
+ * 
+ * The code demonstrates how to use the Bind library to create a graphical user interface (GUI)
+ * for controlling a servo motor using various UI elements such as buttons, knobs, and labels.
+ * 
+ * The servo motor is connected to pin 13 (SERVO_PIN) of the Arduino board.
+ * 
+ * The code also includes platform-specific configurations for different boards such as AVR, ESP32, and Raspberry Pi Pico W.
+ * 
+ * Note: For Raspberry Pi Pico W. Enable Bluetooth using the IDE menu.
+ * Use the 'Tools->IP/Bluetooth Stack' menu in the IDE to enable it (x + Bluetooth).
+ */
 #include "Bind.hpp"
-#if defined(ESP32)
+
+// Include libraries based on the board.
+#ifdef __AVR__
+// Using bluetooth module (like HC-06) with SoftwareSerial.
+// For AVR Arduinos like Pro Mini or Mega: RX: pin 4, TX: pin 3
+#include <SoftwareSerial.h>
+SoftwareSerial bindPort(4, 3);
+#elif defined(ESP32)
+// Using built-in Bluetooth module.
 #include "BluetoothSerial.h"
 #include <ESP32Servo.h>
-BluetoothSerial SerialBT;
+BluetoothSerial bindPort;
 #elif defined(ARDUINO_ARCH_RP2040)
+// Using built-in Bluetooth module.
 #include <Servo.h>
 #include <SerialBT.h>
+#define bindPort SerialBT
 #endif
 
-// Define variables
-int analogInPin = 15;
-int sensorValue = 0;
-int outputValue = 0;
-int counter = 0;
-int r = 4, g = 4, b = 2;
-int ledCount = 0;
-unsigned long lastMs = 0;
-unsigned long deltaMs = 0;
-Servo myservo;
+// Note: Alternativlly you can use any HardwareSerial for the external Bluetooth module,
+// or use USB port (Serial) with an OTG adapter to connect to the phone.
+//#define bindPort Serial // To use HardwareSerial over USB port.
+//#define USE_USB_SERIAL_FOR_BIND  // To use USB port for serial communication.
 
-// Define pin for NeoPixels
-#define PIN 12  // On Trinket or Gemma, suggest changing this to 1
-
-// Define number of NeoPixels
-#define NUMPIXELS 8  // Popular NeoPixel ring size
-
-// If the board doesn't have built-in LED, assign an external LED here (if wish to use it):
+// If the board doesn't have a built-in LED, assign an external LED to the LED_BUILTIN pin (if you want to use it).
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 10
 #endif
 
-#define servoPin 13
+#define SERVO_PIN 13
+
+// Define variables
+Servo myservo;
 
 // Create Bind objects
 Bind bind;
-BindSeekBar seekBar1;
-BindGaugeCompact gauge1;
-BindColorPicker colorPicker1;
+
 BindButton button1;
-BindButton button2;
 BindTextLabel textLabel1;
 BindChart chart1;
 BindKnob knob1;
+BindRectangle rectangle1;
 
 // Callback function for screen setup
-void onConnection(int16_t width, int16_t height) {
+void onConnection(int16_t width, int16_t height)
+{
   Serial.println("Screen setup started!");
   setScreenConfig();
+  addRectangle1(width, height);
   addButton1();
   addKnob();
   addtTextlabel();
   Serial.println("Screen setup done!");
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-
-  // Set analog read resolution for ESP32 and RP2040
-#if defined(ESP32) || defined(ARDUINO_ARCH_RP2040)
-  analogReadResolution(10);
-#endif
-
 #if defined(ESP32)
-  myservo.setPeriodHertz(50);    // standard 50 hz servo
-  myservo.attach(servoPin, 500, 2400); // attaches the servo on pin 18 to the servo object
+  myservo.setPeriodHertz(50);          // standard #if defined(ESP32)
+  myservo.setPeriodHertz(50);          // standard 50 hz servo50 hz servo
+  myservo.attach(SERVO_PIN, 500, 2500); // attaches the servo on pin 18 to the servo object
+#ifndef USE_USB_SERIAL_FOR_BIND
   String devName = "BindOnESP32";
-  SerialBT.begin(devName);
+  bindPort.begin(devName);
   Serial.println("The Bluetooth device started. Pair your phone with Bluetooth!");
   Serial.println("Device Name:");
   Serial.println(devName);
-  analogInPin = 15;
-#elif defined(ARDUINO_ARCH_RP2040)
-  myservo.attach(2);
-  analogInPin = A0;
-  SerialBT.begin();
+#endif // USE_USB_SERIAL_FOR_BIND
+#elif defined(ARDUINO_ARCH_RP2040) // Raspberry Pi Pico W
+  // servo pin is 2 for Raspberry Pi Pico W
+  myservo.attach(SERVO_PIN, 500, 2500);
+#ifndef USE_USB_SERIAL_FOR_BIND
+  bindPort.begin();
+#endif // USE_USB_SERIAL_FOR_BIND
 #endif
 
   pinMode(LED_BUILTIN, OUTPUT);
-
-  // Initialize the Bind object and specify the communication method (SerialBT) and callback function (onConnection).
-  bind.init(SerialBT, onConnection);
-  // Note: It was SerialBT here, but it could be any serial port, including hardware and software serial.
-
+  // Initialize the Bind object and specify the communication method (bindPort) and callback function (onConnection).
+  bind.init(bindPort, onConnection);
+  // Note: It was bindPort here, but it could be any serial port, including hardware and software serial.
   // Connect the callback functions with the Bind objects.
   bind.join(button1, button1_pressed);
-  // bind.join(button2, button2_pressed);
   bind.join(knob1, knob1_changed);
 }
 
-void loop() {
+void loop()
+{
   // Regularly synchronize Bind UI events
   bind.sync();
   // This delay is not an essential part of the code
   // but is included here to simulate a brief pause..
-  //syncPotentiometer();
   delay(10);
 }
