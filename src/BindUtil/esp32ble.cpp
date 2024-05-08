6a81410470f82b1d0b51692f05c374edcf4d85c7
H1Jam <46633362+H1Jam@users.noreply.github.com>
2024-05-08 00:40:13 -0400

    Create esp32ble.cpp

---
+++ src/BindUtil/esp32ble.cpp
@@ -0,0 +1,54 @@
+#include "esp32ble.h"
+#ifdef ARDUINO_ARCH_ESP32
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+void sendData(uint8_t *data, size_t size)
+{
+    if (deviceConnected)
+    {
+        pTxCharacteristic->setValue(data, size);
+        pTxCharacteristic->notify();
+        delay(2); // bluetooth stack will go into congestion, if too many packets are sent
+    }
+}
+
+BleStream* ble_init(Bind &bind, std::string deviceName)
+{
+
+    BLEDevice::init(deviceName);
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new BleCallbacks());
+
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    pTxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_TX,
+        BLECharacteristic::PROPERTY_NOTIFY);
+
+    pTxCharacteristic->addDescriptor(new BLE2902());
+
+    BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_RX,
+        BLECharacteristic::PROPERTY_WRITE);
+
+    pRxCharacteristic->setCallbacks(new BleRXCallbacks(bind));
+
+    pService->start();
+
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);
+    pAdvertising->setMaxPreferred(0x12);
+    BLEDevice::startAdvertising();
+    return new BleStream(sendData);
+}
+
+#endif // ARDUINO_ARCH_ESP32
