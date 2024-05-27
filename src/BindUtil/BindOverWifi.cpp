edab71b01b905f2adfc5137f2846a2a373c0287e
H1Jam <46633362+H1Jam@users.noreply.github.com>
2024-05-27 03:20:10 -0400

    rename udp to wifi

--- src/BindUtil/BindOverWifi.cpp
+++ src/BindUtil/BindOverWifi.cpp
@@ -1,10 +1,51 @@
-#include "BindOverUDP.h"
+#include "BindOverWifi.h"
 
 uint8_t UDPStream::discoveryMsg[6] = { 0x42, 0X69, 0X6E, 0X64, 0X44, 0X43 };   // BindDC
 uint8_t UDPStream::connectMsg[6] = { 0x42, 0X69, 0X6E, 0X64, 0X43, 0X4E };     // BindCN
 uint8_t UDPStream::stopMsg[6] = { 0x42, 0X69, 0X6E, 0X64, 0X53, 0X54 };        // BindST
 uint8_t UDPStream::bindHeartbeat[6] = { 0x42, 0X69, 0X6E, 0X64, 0X48, 0X42 };  // BindHB
 uint16_t UDPStream::bindPort = 7997;
+
+bool UDPStream::begin(Bind &bind, const char* bindname) {
+  return begin(bindname, bind);
+}
+
+bool UDPStream::begin(const char* bindname, Bind &bind) {
+  this->_bind = &bind;
+  this->bindname = bindname;
+  if (this->udp.listen(this->bindPort)) {
+    this->udp.onPacket([this](AsyncUDPPacket packet) {
+       this->handleUDP(*this, packet);
+    });
+    return true;
+  }
+  return false;
+}
+
+size_t UDPStream::write(const uint8_t *buffer, size_t size) {
+  if (!this->sendPackets) {
+    return 0;
+  }
+
+  if (this->sendPackets == true && this->lastHeartbeat + 5000 > millis()) {
+    this->udp.writeTo(buffer, size, this->canvasIP, this->bindPort);
+    #if UDP_DEBUG_MSG
+    Serial.print("TX: ");
+    Serial.print(this->canvasIP);
+    Serial.print(" - Hex[");
+    for (int i = 0; i < size; i++) {
+      Serial.print(buffer[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println("]");
+    #endif
+    return size;
+  } else {
+    this->sendPackets = false;
+    this->canvasIP = IPAddress(0, 0, 0, 0);
+    return 0;
+  }
+}
 
 void UDPStream::handleUDP(UDPStream& udpStream, AsyncUDPPacket& packet) {
   #if UDP_DEBUG_MSG
---
+++ src/BindUtil/BindOverWifi.h
@@ -0,0 +1,39 @@
+#ifndef BINDOVERUDP_H
+#define BINDOVERUDP_H
+
+#include <Arduino.h>
+#include <Stream.h>
+#include <AsyncUDP.h>
+#include "Bind.h"
+#define UDP_DEBUG_MSG 0 // Set to 1 to enable debug messages
+
+/*
+  * This class is used to send and receive data over UDP to be used 
+  * with the Bind class.
+*/
+class UDPStream : public Stream
+{
+private:
+    bool sendPackets = false;
+    static uint8_t discoveryMsg[];
+    static uint8_t connectMsg[];
+    static uint8_t stopMsg[];
+    static uint8_t bindHeartbeat[];
+    static uint16_t bindPort;
+    IPAddress canvasIP = IPAddress(0, 0, 0, 0);
+    void handleUDP(UDPStream& udpStream, AsyncUDPPacket& packet);
+    Bind *_bind;
+    AsyncUDP udp;
+    uint32_t lastHeartbeat = 0;
+    const char* bindname;
+public:
+    bool begin(const char* bindname, Bind &bind);
+    bool begin(Bind &bind, const char* bindname);
+    size_t write(const uint8_t *buffer, size_t size) override;
+    int available() override { return 0; }
+    int read() override { return 0; }
+    int peek() override { return 0; }
+    void flush() override {}
+    size_t write(uint8_t) override { return 0; }
+};
+#endif // BINDOVERUDP_H
