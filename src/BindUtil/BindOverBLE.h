2117c188b8d1a68452c1d34352bb71ad0088da9f
H1Jam <46633362+H1Jam@users.noreply.github.com>
2024-05-28 01:24:19 -0400

    Update BindOverBLE.h

--- src/BindUtil/BindOverBLE.h
+++ src/BindUtil/BindOverBLE.h
@@ -2,43 +2,106 @@
 #define __ESP32BLE_H__
 #ifdef ARDUINO_ARCH_ESP32
 
+#include "Bind.h"
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <Arduino.h>
 #include "Stream.h"
-#include "Bind.h"
 
-extern BLEServer *pServer;
-extern BLECharacteristic *pTxCharacteristic;
-extern bool deviceConnected;
-extern bool oldDeviceConnected;
+
+class BleCallbacks : public BLEServerCallbacks
+{
+private:
+    bool *_deviceConnected;
+public:
+    // Get _deviceConnected pointer
+    BleCallbacks(bool &deviceConnected) : _deviceConnected(&deviceConnected){
+        
+    }
+
+    void onConnect(BLEServer *pServer)
+    {
+        *this->_deviceConnected = true;
+    }
+
+    void onDisconnect(BLEServer *pServer)
+    {
+        *this->_deviceConnected = false;
+        delay(500);                  // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+    }
+};
+
+class BleRXCallbacks : public BLECharacteristicCallbacks
+{
+private:
+    Bind *_bind;
+
+public:
+    BleRXCallbacks(Bind &bind)
+    {
+        this->_bind = &bind;
+    }
+
+    void onWrite(BLECharacteristic *pCharacteristic) override
+    {
+        uint8_t *data = pCharacteristic->getData();
+        size_t dataln = pCharacteristic->getLength();
+        if (dataln > 0)
+        {
+            this->_bind->sync(data, dataln);
+        }
+    }
+};
 
 /**
  * @brief esp32 BLE stream class.
- * 
+ *
  * @details This class is used to create a stream object that can be used to
  * send data over BLE.
- * 
- * @param writeCallback The callback function that will be called when need to 
+ *
+ * @param writeCallback The callback function that will be called when need to
  * write data to the stream.
- * 
+ *
  * @return The stream object.
  */
 class BleStream : public Stream
 {
 private:
-    void (*_writeCallback)(uint8_t *, size_t);
-public:
-    BleStream(void (*writeCallback)(uint8_t *, size_t))
-    {
-        _writeCallback = writeCallback;
+    BLEServer *pServer = NULL;
+    BLECharacteristic *pTxCharacteristic;
+    bool deviceConnected = false;
+    bool oldDeviceConnected = false;
+    BleRXCallbacks *rxCallbacks = NULL;
+    BleCallbacks *bleCallbacks = NULL;
 
-    }
+public:
+    /**
+     * @brief Initialize the BLE service.
+     *
+     * @details This function is used to initialize a BLE stream.
+     * You can pass the returning object to bind.init function.
+     *
+     * @param bind The bind object.
+     * @param bindname The name of the device.
+     *
+     * @return The BLE stream object.
+     */
+    bool begin(const char *bindname, Bind &bind);
+
+    // Same as above but with different order of arguments
+    bool begin(Bind &bind, const char *bindname);
     size_t write(const uint8_t *buffer, size_t size) override
     {
-        _writeCallback((uint8_t *)buffer, size);
+        if (this->deviceConnected)
+        {
+            this->pTxCharacteristic->setValue((uint8_t *)buffer, size);
+            this->pTxCharacteristic->notify();
+            // Add a small delay to prevent congestion in the Bluetooth stack.
+            delay(10);
+        }
         return size;
     }
     int available() override { return 0; }
@@ -48,55 +111,8 @@ public:
     size_t write(uint8_t) override { return 0; }
 };
 
-class BleCallbacks : public BLEServerCallbacks
-{
-    void onConnect(BLEServer *pServer)
-    {
-        deviceConnected = true;
-    };
+// void sendData(uint8_t *data, size_t size);
 
-    void onDisconnect(BLEServer *pServer)
-    {
-        deviceConnected = false;
-        delay(500);                  // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-    }
-};
-
-void sendData(uint8_t *data, size_t size);
-
-class BleRXCallbacks : public BLECharacteristicCallbacks
-{
-private:
-     Bind *_bind;
-public:
-    BleRXCallbacks(Bind &bind)
-    {
-        this->_bind = &bind;
-    }
-
-    void onWrite(BLECharacteristic *pCharacteristic)
-    {
-        uint8_t *data = pCharacteristic->getData();
-        size_t dataln = pCharacteristic->getLength();
-        if (dataln > 0)
-        {
-             this->_bind->sync(data, dataln);
-        }
-    }
-};
-
-/**
- * @brief Initialize the BLE service.
- * 
- * @details This function is used to initialize a BLE stream. 
- * You can pass the returning object to bind.init(...,...) function.
- * 
- * @param bind The bind object.
- * 
- * @return The BLE stream object.
- */
-BleStream* ble_init(Bind &bind, std::string deviceName);
 
 #endif // ARDUINO_ARCH_ESP32
 #endif // __ESP32BLE_H__
