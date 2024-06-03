f27a33423cbd75664af5a5c20ddaf54023f6df79
H1Jam <46633362+H1Jam@users.noreply.github.com>
2024-06-03 02:14:22 -0400

    Change BLE MTU to 64

--- src/BindUtil/BindOverBLE.cpp
+++ src/BindUtil/BindOverBLE.cpp
@@ -9,6 +9,7 @@ bool BleStream::begin(const char *deviceName, Bind &bind)
 {
     this->rxCallbacks = new BleRXCallbacks(bind);
     BLEDevice::init(std::string(deviceName));
+    BLEDevice::setMTU(64);
     this->pServer = BLEDevice::createServer();
     this->bleCallbacks = new BleCallbacks(this->deviceConnected);
     this->pServer->setCallbacks(this->bleCallbacks);
