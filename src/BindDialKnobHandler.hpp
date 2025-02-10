491d892094836e09e18b6a3fae2744171bf4421e
H1Jam <46633362+H1Jam@users.noreply.github.com>
2025-02-09 20:06:08 -0500

    Update BindDialKnobHandler.hpp

--- src/BindDialKnobHandler.hpp
+++ src/BindDialKnobHandler.hpp
@@ -1,16 +1,16 @@
 #ifndef __BINDDIALKNOBHANDLER_H
 #define __BINDDIALKNOBHANDLER_H
 #include "Stream.h" // for NULL
+#include "BindKnob.hpp"
 // TODO: Add the class summaries!
 class DialKnobHandler
 {
 private:
-  int16_t *value = NULL;
-  void (*changeCallback)(int16_t);
+  BindKnob *screenKnob = NULL;
+  void (*changeCallback)(int16_t) = NULL;
 
 public:
-  DialKnobHandler();
-  DialKnobHandler(int16_t *val, void (*_changeCallback)(int16_t));
+  DialKnobHandler(BindKnob *screenKnob = NULL, void (*_changeCallback)(int16_t) = NULL);
   void changed(int16_t val);
 };
 #endif /* __BINDDIALKNOBHANDLER_H */
