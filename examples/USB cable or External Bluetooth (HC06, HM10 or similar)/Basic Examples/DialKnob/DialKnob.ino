ce135e6e93f2aaf36dc08a967e56fb332d3e715b
H1Jam <46633362+H1Jam@users.noreply.github.com>
2025-02-17 17:38:45 -0500

    Update DialKnob.ino

--- examples/USB cable or External Bluetooth (HC06, HM10 or similar)/Basic Examples/DialKnob/DialKnob.ino
+++ examples/USB cable or External Bluetooth (HC06, HM10 or similar)/Basic Examples/DialKnob/DialKnob.ino
@@ -11,35 +11,14 @@
 Bind bind;
 BindKnob knob1;
 BindKnob knob2;
-const int ledPin = 2;
 
-// Screen Setup Callback for BindCanvas
-void onConnection(int16_t w, int16_t h) {
-  addKnobs();
-}
+// if the LED_BUILTIN is not defined by the board, define it as pin 2
+#ifndef LED_BUILTIN 
+#define LED_BUILTIN 2
+#endif
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-
-  // Initialize the Bind object and specify the communication method (Serial) and callback function (onConnection).
-	bind.init(Serial, onConnection);
-
-  // Connect the callback functions with the Bind objects.
-  bind.join(knob1, knob1_changed);
-  bind.join(knob2, knob2_changed);
-
-}
-
-void loop() {
-  // Synchronize Bind UI Events (Not required on ESP32 when using BindOverBLE or BindOverWifi)
-  // Callin bind.sync() regularly is crucial to handle of user inputs and touch events.
-  // It's recommended to call bind.sync() a couple of times per second, but the faster, the better!
-  bind.sync();
-
-  // Do some work...
-  delay(10);
-}
+const int ledPin = LED_BUILTIN; // change this to the pin where your LED is connected.
+unsigned long lastMs = 0;
 
 // Callback for Knob 1 Value Change
 void knob1_changed(int16_t val) {
@@ -52,9 +31,13 @@ void knob1_changed(int16_t val) {
   }
 }
 
+// Atention: If you are using this Serial port for Bind,
+// you should comment all Serial.print(...)/println(...) lines to avoid conflicts.
+
 // Callback for Knob 2 Value Change
 void knob2_changed(int16_t val) {
-
+  Serial.print("Knob 2 value changed to: ");
+  Serial.println(val);
   // Implement your custom actions here based on the knob value
 }
 
@@ -69,6 +52,7 @@ void addKnobs() {
   knob1.maxValue = 100;
   knob1.value = 50;  // Initial value
   knob1.setlabel("knob1");
+  knob1.setCallback(knob1_changed); // Set the callback function for the knob1 object.
   bind.sync(knob1);
 
   // Configure and synchronize Knob 2
@@ -80,5 +64,36 @@ void addKnobs() {
   knob2.maxValue = 100;
   knob2.value = 25;  // Initial value
   knob2.setlabel("knob2");
+  knob2.setCallback(knob2_changed); // Set the callback function for the knob2 object.
   bind.sync(knob2);
+}
+
+// Screen Setup Callback for BindCanvas
+void onConnection(int16_t w, int16_t h) {
+  addKnobs();
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  // Initialize the Bind object and specify the communication method (Serial) and callback function (onConnection).
+	bind.init(Serial, onConnection);
+}
+
+void loop() {
+  // Synchronize Bind UI Events (Not required on ESP32 when using BindOverBLE or BindOverWifi)
+  // Callin bind.sync() regularly is crucial to handle of user inputs and touch events.
+  // It's recommended to call bind.sync() a couple of times per second, but the faster, the better!
+  bind.sync();
+  // Do some work...
+  delay(10);
+  if (millis() - lastMs > 1000) {
+    lastMs = millis();
+    // Print the current values of the knobs
+    Serial.print("Knob 1 value: ");
+    Serial.print(knob1.value);
+    Serial.print("\tKnob 2 value: ");
+    Serial.println(knob2.value);
+  }
 }
