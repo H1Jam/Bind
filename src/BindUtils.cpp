78f49170b4446cd5784a550450388ce42acafdd3
H1Jam <46633362+H1Jam@users.noreply.github.com>
2025-02-27 21:20:21 -0500

    Update BindUtils.cpp

--- src/BindUtils.cpp
+++ src/BindUtils.cpp
@@ -6,3 +6,17 @@ void copyAndOffset(uint8_t *out, uint16_t *offset, const void *source, size_t nu
   memcpy(out + (*offset), source, num);
   *offset += num;
 }
+
+void copyStringWithLength(uint8_t *out, uint16_t *offset, const char *str)
+{
+    uint8_t length = strlen(str);
+    if (length > MAX_STRING_LENGTH_TERMINAL)
+    {
+        length = MAX_STRING_LENGTH_TERMINAL;
+    }
+    out[(*offset)++] = length;
+    if (length > 0)
+    {
+        copyAndOffset(out, offset, str, length);
+    }
+}
