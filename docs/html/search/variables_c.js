99369cb0d9e15086b9820d88f15695623a04c32e
H1Jam <46633362+H1Jam@users.noreply.github.com>
2025-03-02 23:57:37 -0500

    Update variables_c.js

--- docs/html/search/variables_c.js
+++ docs/html/search/variables_c.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['scale_0',['scale',['../class_bind_map_marker.html#a514a3626f105db11354d48cb4294d2c8',1,'BindMapMarker']]],
   ['screenorientation_1',['screenOrientation',['../class_bind_canvas_settings.html#a94af8f4312e0394bafe409b16ae15c62',1,'BindCanvasSettings']]],
-  ['springed_2',['springed',['../class_bind_joystick.html#ac5ae4475e144d0a1a81f7173f226ed8b',1,'BindJoystick']]],
-  ['style_3',['style',['../class_bind_gauge_simple.html#a1202f81ef39b2c1229bbe60d2491f58e',1,'BindGaugeSimple']]],
-  ['sx_4',['sX',['../class_bind_joystick.html#a0dd8b4eea5818182ec452d9918cf73b7',1,'BindJoystick']]],
-  ['sy_5',['sY',['../class_bind_joystick.html#aea7d2a50317a32aa1cc6f437a0b76f44',1,'BindJoystick']]]
+  ['singlebutton_2',['singleButton',['../class_bind_dialog.html#a046dff94e341e9350b4cc60e785cec04',1,'BindDialog']]],
+  ['springed_3',['springed',['../class_bind_joystick.html#ac5ae4475e144d0a1a81f7173f226ed8b',1,'BindJoystick']]],
+  ['style_4',['style',['../class_bind_gauge_simple.html#a1202f81ef39b2c1229bbe60d2491f58e',1,'BindGaugeSimple']]],
+  ['sx_5',['sX',['../class_bind_joystick.html#a0dd8b4eea5818182ec452d9918cf73b7',1,'BindJoystick']]],
+  ['sy_6',['sY',['../class_bind_joystick.html#aea7d2a50317a32aa1cc6f437a0b76f44',1,'BindJoystick']]]
 ];
