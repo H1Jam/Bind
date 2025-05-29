046293f8f3731ce10d99387e26267d5be1b70dba
H1Jam <46633362+H1Jam@users.noreply.github.com>
2025-05-28 22:01:25 -0400

    Update BindOverBLE.cpp

--- src/BindUtil/BindOverBLE.cpp
+++ src/BindUtil/BindOverBLE.cpp
@@ -1,6 +1,6 @@
 #ifdef ARDUINO_ARCH_ESP32
-#ifndef WIFI_ONLY_BIND
 #include "BindOverBLE.h"
+#ifndef BIND_DISABLE_BLE
 #define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -63,5 +63,5 @@ void BleRXCallbacks::onWrite(BLECharacteristic *pCharacteristic)
         this->_bind->sync(pCharacteristic->getData(), dataln);
     }
 }
-#endif // WIFI_ONLY_BIND
+#endif // BIND_DISABLE_BLE
 #endif // ARDUINO_ARCH_ESP32
